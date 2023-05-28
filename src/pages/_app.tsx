8c1f80dfb847f095691913dccc156eabe300cabd
Chris Bautista <chrisbautista@netflix.com>
2023-05-28 00:01:51 -0700

    remove dead code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { type Session } from "next-auth";
 
 import { type AppType } from "next/app";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps,
-}) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
