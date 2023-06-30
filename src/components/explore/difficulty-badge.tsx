6f418c075c69f2e22e52a58cf369a18dd30de108
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-30 17:11:43 -0400

    explore-card: adjust difficulty colors for light mode

--- src/components/explore/difficulty-badge.tsx
+++ src/components/explore/difficulty-badge.tsx
@@ -6,11 +6,11 @@ interface Props {
 }
 
 const COLORS_BY_DIFFICULTY = {
-  BEGINNER: 'bg-pink-300',
-  EASY: 'bg-green-300',
-  MEDIUM: 'bg-yellow-300',
-  HARD: 'bg-red-300',
-  EXTREME: 'bg-orange-300',
+  BEGINNER: 'bg-pink-500 dark:bg-pink-300',
+  EASY: 'bg-green-500 dark:bg-green-300',
+  MEDIUM: 'bg-yellow-500 dark:bg-yellow-300',
+  HARD: 'bg-red-500 dark:bg-red-300',
+  EXTREME: 'bg-orange-500 dark:bg-orange-300',
 };
 export function DifficultyBadge({ difficulty }: Props) {
   return (
