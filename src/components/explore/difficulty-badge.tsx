e893cb2cd6368afe08ab9b08e0b107c694b2be27
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-30 17:19:44 -0400

    difficulty-badge: fix dark mode bug

--- src/components/explore/difficulty-badge.tsx
+++ src/components/explore/difficulty-badge.tsx
@@ -6,18 +6,12 @@ interface Props {
 }
 
 const COLORS_BY_DIFFICULTY = {
-  BEGINNER: 'bg-pink-500 dark:bg-pink-300',
-  EASY: 'bg-green-500 dark:bg-green-300',
-  MEDIUM: 'bg-yellow-500 dark:bg-yellow-300',
-  HARD: 'bg-red-500 dark:bg-red-300',
-  EXTREME: 'bg-orange-500 dark:bg-orange-300',
+  BEGINNER: 'dark:bg-pink-300 bg-pink-500',
+  EASY: 'dark:bg-green-300 bg-green-500',
+  MEDIUM: 'dark:bg-yellow-300 bg-yellow-500',
+  HARD: 'dark:bg-red-300 bg-red-500',
+  EXTREME: 'dark:bg-orange-300 bg-orange-500',
 };
 export function DifficultyBadge({ difficulty }: Props) {
-  return (
-    <Badge
-      className={`${COLORS_BY_DIFFICULTY[difficulty]} hover:${COLORS_BY_DIFFICULTY[difficulty]}}`}
-    >
-      {difficulty}
-    </Badge>
-  );
+  return <Badge className={`${COLORS_BY_DIFFICULTY[difficulty]}`}>{difficulty}</Badge>;
 }
