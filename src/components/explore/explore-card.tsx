d8b42c5180ccd006881baeb5bcdb61f9f6107e26
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-30 20:18:17 -0400

    explore-card: fix description fade

--- src/components/explore/explore-card.tsx
+++ src/components/explore/explore-card.tsx
@@ -52,11 +52,7 @@ export function ExploreCard({ challenge }: Props) {
         <div className="space-y-1">
           <CardTitle>{challenge.name}</CardTitle>
           <CardDescription className="relative h-48 overflow-hidden pb-4">
-            {/* it didn't like the div */}
-            {/* <div
-              className="pointer-events-none absolute inset-0 h-full w-full"
-              style={{ boxShadow: 'inset 0 -1.5rem 1rem -0.5rem #000' }}
-            ></div> */}
+            <div className="pointer-events-none absolute inset-0 h-full w-full shadow-[inset_0_-1.5rem_1rem_-0.5rem_hsl(var(--card))]"></div>
             {challenge.description}
           </CardDescription>
         </div>
