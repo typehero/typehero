28b20ec83d8a602229e3e74eb367947d5789793b
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-06-30 21:20:26 -0400

    temp logo stuff (#25)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <head />
+      <head>
+        <title>Type Hero</title>
+        <meta
+          name="description"
+          content="Level up your typescript skills with interactive exercises"
+        />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={`${inter.className} flex flex-col`}>
         <Providers>
           <Navigation />
