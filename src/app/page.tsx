20be51eafdd245842613a3a4ef73eafd1129721d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-02 14:56:26 -0500

    More info on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth';
 import Head from 'next/head';
 import { SignInOutButton } from '~/components/ui/sign-in-out-button';
+
 import { authOptions } from '~/server/auth';
 
 export default async function Index() {
@@ -8,19 +9,33 @@ export default async function Index() {
   return (
     <>
       <Head>
-        <title>Type Hero</title>
+        <title>Typehero</title>
         <meta
           name="description"
           content="Level up your typescript skills with interactive exercises"
         />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-center text-2xl text-white">
-          {session && <span>Logged in as {session.user?.name}</span>}
+      <div className="mt-10 flex flex-col items-center justify-center gap-4">
+        <h1 className="text-center text-6xl text-white">Typehero</h1>
+        <p className="font-bold">Challenage. Learn. Excel.</p>
+
+        <p className="mt-4 text-white">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+          architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+          voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+          amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
+          labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+          consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+          nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
         </p>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <SignInOutButton session={session} />
+        </div>
       </div>
-      <SignInOutButton session={session} />
     </>
   );
 }
--- src/components/ui/sign-in-out-button.tsx
+++ src/components/ui/sign-in-out-button.tsx
@@ -7,7 +7,7 @@ export function SignInOutButton({ session }: { session: Session | null }) {
   return (
     <button
       className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-      onClick={session ? () => void signOut() : () => void signIn()}
+      onClick={session ? () => void signOut() : () => void signIn('github')}
     >
       {session ? 'Sign out' : 'Sign in'}
     </button>
