f44dc15e0321497b990d0b0320ab16377807e62a
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-02 15:50:21 -0400

    fix sign-in-out button light mode

--- src/components/ui/sign-in-out-button.tsx
+++ src/components/ui/sign-in-out-button.tsx
@@ -8,6 +8,8 @@ export function SignInOutButton({ session }: { session: Session | null }) {
     <button
       className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
       onClick={session ? () => void signOut() : () => void signIn('github')}
+      className="rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-black/20 dark:bg-white/10 dark:text-white dark:hover:bg-white/20"
+      onClick={session ? () => void signOut() : () => void signIn()}
     >
       {session ? 'Sign out' : 'Sign in'}
     </button>
