d3a3a6ffea991a9883e81383e60ec1fd528c3c55
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-02 16:32:57 -0400

    yeet sign in button into navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { getServerSession } from 'next-auth';
 import Head from 'next/head';
-import { SignInOutButton } from '~/components/ui/sign-in-out-button';
-
 import { authOptions } from '~/server/auth';
 
 export default async function Index() {
