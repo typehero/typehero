2b80970c7d4c61fb11c844d8397e97808b95cd39
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-04 11:14:31 -0500

    Fix lint errors

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,5 @@
-"use client";
-import * as React from "react";
+'use client';
+import * as React from 'react';
 
 import { cn } from '~/utils/cn';
 
--- src/components/user/profile.tsx
+++ src/components/user/profile.tsx
@@ -1,6 +1,5 @@
 import { notFound } from 'next/navigation';
 import { prisma } from '~/server/db';
-import Image from 'next/image';
 import DashboardPage from '../dashboard/DashboardPage';
 
 interface Props {
@@ -20,18 +19,5 @@ export async function Profile({ username: usernameFromQuery }: Props) {
     notFound();
   }
 
-  return (
-    <DashboardPage />
-    // <h1>{user.id}</h1>
-    // <Image alt="user avatar" width="100" height="100" src={user.image ?? ''} />
-
-
-    // <p>{user.email}</p>
-
-    // <h2 className="text-xl">Raw user:</h2>
-    // <pre>{JSON.stringify(user, null, 4)}</pre>
-    //     {/* <button onClick={() => handleClick(user.id)}>update user name for id: {user.id}</button> */}
-    //   </>
-
-  );
+  return <DashboardPage />;
 }
