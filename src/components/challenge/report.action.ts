9923138ab2dd047997c868a2449c5851e6a71f3c
Jim Burbridge <jhechtf@gmail.com>
2023-07-04 14:20:00 -0700

    feat: adds reporting UI on challenges (#64)
    Closes #7

---
+++ src/components/challenge/report.action.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { prisma } from '~/server/db';
+import type { FormValues } from './description-panel';
+
+export async function doTheThing() {
+  await Promise.resolve(0);
+  return 'done';
+}
+
+export async function addReport(challengeId: number, userId: string, data: FormValues) {
+  if(userId === undefined) return 'not_logged_in';
+
+  const report = await prisma.report.findMany({
+    where: {
+      challengeId,
+      userId,
+    }
+  });
+  if(report.length > 0) {
+    console.info(report);
+    return 'report_already_made';
+  }
+  
+  await prisma.report.create({
+    data: {
+      challengeId,
+      userId,
+      text: data.comments,
+      unclear: data.examples,
+      derogatory: data.derogatory,
+    }
+  })
+
+  return 'created';
+}
---
+++ src/components/ui/action-menu.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react';
+import { 
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from './dropdown-menu';
+
+import * as AllIcons from 'lucide-react';
+import type { Icon } from 'lucide-react';
+
+export type ActionMenuItem = {
+  icon?: keyof typeof AllIcons;
+  label: string;
+  key: string;
+};
+
+export interface ActionMenuProps {
+  items: ActionMenuItem[];
+  onChange: (e: ActionMenuItem) => void;
+}
+
+type OptionalIcon = Icon | null;
+
+export function ActionMenu(props: ActionMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <AllIcons.MoreHorizontal size={16} />        
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>       
+        {
+          props.items.map(item => {
+            const Icon = (item.icon ? AllIcons[item.icon] : null) as OptionalIcon;
+            return (
+              <DropdownMenuItem onClick={() => props.onChange(item)} key={item.key} className="p-2 flex gap-2 items-center">
+                <div>
+                  {Icon ? <Icon size={16} /> : null}
+                </div>
+                <div>
+                  {item.label}
+                </div>
+              </DropdownMenuItem>
+            );
+          })
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
