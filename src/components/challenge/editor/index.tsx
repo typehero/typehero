6b0354c022b302c8f0a969a8136c7a4ddaccbf80
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-03 19:54:33 -0500

    Add todo comment (#60)

--- src/components/challenge/editor/index.tsx
+++ src/components/challenge/editor/index.tsx
@@ -134,8 +134,10 @@ export const CodePanel = ({ challenge }: Props) => {
           onError(hasErrors);
         };
 
+        // TODO: we prolly should use this for blocking ranges as it might not be as janky
+        // https://github.com/Pranomvignesh/constrained-editor-plugin
         model.onDidChangeContent((e) => {
-        // in monaco editor, the first line is e1e1e
+          // in monaco editor, the first line is e1e1e
           // do net let them type if they are editing before lineWithUserCode
           if (e.changes.some((c) => c.range.startLineNumber <= (lineWithUserCode + 1))) {
             editor.trigger('someIdString', 'undo', null);
