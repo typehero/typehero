eeedfcdb361a2b95265d5b78005b65c8d1911fda
ggrandi <giovannigrandi04@gmail.com>
2023-07-06 22:21:31 +0200

    updated typescript

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
  * for Docker builds.
  */
 await import('./src/env.mjs');
+await import('./scripts/dl-monaco.mjs').then(({ download }) => download());
 
 /** @type {import("next").NextConfig} */
 const config = {
