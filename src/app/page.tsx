baa9c6cb3a428f1340905855844b5030d8b248e5
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-05 21:10:15 -0400

    footer on landing page, resolves #98

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Footsies } from '~/components/ui/footsies';
 
 export default function Index() {
   return (
@@ -12,11 +13,17 @@ export default function Index() {
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
       <div
-        className="relative flex flex-col items-center justify-center gap-4 overflow-hidden px-4"
+        className="relative mb-[56px] flex flex-col items-center justify-center gap-4 overflow-x-hidden overflow-y-visible px-4"
         style={{ minHeight: 'calc(100dvh - 112px)' }}
       >
-        <div className="absolute left-1/2 top-1/2 -z-10 h-96 w-96 -translate-x-[30%] -translate-y-[69%] rounded-full bg-black/0 blur-3xl dark:bg-white/20"></div>
-        <div className="absolute right-1/2 top-1/2 -z-10 h-96 w-96 -translate-y-[69%] translate-x-[30%] rounded-full bg-[#3178c6]/0 blur-3xl dark:bg-[#3178c6]/20"></div>
+        <div
+          id="leftBall"
+          className="absolute left-1/2 top-1/2 -z-10 h-64 w-64 -translate-x-[30%] -translate-y-[69%] rounded-full bg-black/0 blur-3xl dark:bg-white/20 lg:h-96 lg:w-96"
+        ></div>
+        <div
+          id="riteBall"
+          className="absolute right-1/2 top-1/2 -z-10 h-64 w-64 -translate-y-[69%] translate-x-[30%] rounded-full bg-[#3178c6]/0 blur-3xl dark:bg-[#3178c6]/20 lg:h-96 lg:w-96"
+        ></div>
         <h1 className="bg-gradient-to-r from-[#3178c6] to-black bg-clip-text text-center text-9xl font-black text-transparent dark:to-white ">
           Type
           <br />
@@ -29,6 +36,7 @@ export default function Index() {
           illum libero dignissimos, voluptas eius molestiae ipsum!
         </p>
       </div>
+      <Footsies />
     </>
   );
 }
