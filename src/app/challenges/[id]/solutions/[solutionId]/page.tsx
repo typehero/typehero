7186053921c43bf66c1e1cd20b49471f0f967a15
Chris Bautista <chrisbautista@netflix.com>
2023-07-07 14:55:11 -0700

    rename

--- src/app/challenges/[id]/solutions/[solutionId]/page.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  params: {
-    solutionId: string;
-  };
-}
-export default function SolutionPage({ params: { solutionId } }: Props) {
-  return <div>Solution Details for Solution {solutionId}</div>;
-}
