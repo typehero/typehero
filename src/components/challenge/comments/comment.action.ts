955cd36de18e85f98e5490a855fa0c0165aaa510
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-07-09 19:40:49 +0530

    chore: added a few comments.

--- src/components/challenge/comments/comment.action.ts
+++ src/components/challenge/comments/comment.action.ts
@@ -5,12 +5,20 @@ import { CommentReportSchemaType } from '~/components/challenge/comments/comment
 import { authOptions } from '~/server/auth';
 import { prisma } from '~/server/db';
 
+/**
+ * The function creates a comments with given
+ * text and challenge id.
+ * @param challengeId The challenge Id.
+ * @param text The comment text.
+ */
 export async function addChallengeComment(challengeId: number, text: string) {
   const session = await getServerSession(authOptions);
 
+  // 1. Checks.
   if (!session?.user.id) return 'unauthorized';
   if (text.length === 0) return 'text_is_empty';
 
+  // 2. Create a new comment.
   return await prisma.challengeComment.create({
     data: {
       challengeId,
@@ -20,6 +28,13 @@ export async function addChallengeComment(challengeId: number, text: string) {
   });
 }
 
+/**
+ * Creates a new report on a challenge comment with a
+ * few parameters describing the report.
+ * @param formValues Accepts {@link CommentReportSchemaType}.
+ * @param commentId The comment id.
+ * @returns
+ */
 export async function reportChallengeComment(
   formValues: CommentReportSchemaType,
   commentId: number,
@@ -27,7 +42,7 @@ export async function reportChallengeComment(
   const session = await getServerSession(authOptions);
   const { bullying, hate_speech, spam, text, threat } = formValues;
 
-  // 1. Checks
+  // 1. Checks.
   if (!session?.user.id) return 'unauthorized';
 
   // 2. If report already exist's return.
