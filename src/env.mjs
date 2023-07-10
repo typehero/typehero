9499bc920abd684512597cb22f1d6070c1501357
bautistaaa <chrisbautistaaa@gmail.com>
2023-07-09 20:15:45 -0700

    fix env (#178)
    * fix env
    
    * lint stage + env changes
    
    * oops
    
    * bruh
    
    * add typechecking

--- src/env.mjs
+++ src/env.mjs
@@ -11,8 +11,15 @@ export const env = createEnv({
     NODE_ENV: z.enum(['development', 'test', 'production']),
     GITHUB_ID: z.string(),
     GITHUB_SECRET: z.string(),
-    // EDGE_CONFIG: z.string(),
-    NEXTAUTH_SECRET: z.string(),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === 'production' ? z.string().min(1) : z.string().min(1).optional(),
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      (str) => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string().min(1) : z.string().url(),
+    ),
   },
 
   /**
@@ -33,7 +40,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     GITHUB_ID: process.env.GITHUB_ID,
-    // EDGE_CONFIG: process.env.EDGE_CONFIG,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   /**
