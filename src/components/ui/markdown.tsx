7e7616864265dafdf86700ebdb3a0994bbb03f64
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-11 17:52:08 -0500

    Don't render html comments

--- src/components/ui/markdown.tsx
+++ src/components/ui/markdown.tsx
@@ -5,11 +5,39 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import remarkGfm from 'remark-gfm';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import clsx from 'clsx';
+import { visit, SKIP, type BuildVisitor } from 'unist-util-visit';
+import type { Transformer } from 'unified';
+
+const HTML_COMMENT_REGEX = new RegExp('<!--([\\s\\S]*?)-->', 'g');
+
+/**
+ * Remove HTML comments from Markdown
+ */
+function removeHtmlComments(): Transformer {
+  return (tree) => {
+    // TODO: PRs are welcomed to fix the any type
+    const handler: BuildVisitor<any> = (node, index, parent) => {
+      const isComment = node.value.match(HTML_COMMENT_REGEX);
+
+      if (isComment) {
+        // remove node
+        parent.children.splice(index, 1);
+        // Do not traverse `node`, continue at the node *now* at `index`. http://unifiedjs.com/learn/recipe/remove-node/
+        return [SKIP, index];
+      }
+    };
+
+    visit(tree, 'html', handler);
+
+    // TODO: is this needed
+    visit(tree, 'jsx', handler);
+  };
+}
 
 export function Markdown({ children }: { children: string }) {
   return (
     <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkGfm, removeHtmlComments]}
       components={{
         h1: ({ className, ...props }) => (
           <h1 className={clsx(className, 'mb-2 pb-2 text-3xl font-bold')} {...props} />
