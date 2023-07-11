8b52affff58eb3685c53399e82cf3439bcf8c105
bautistaaa <chrisbautistaaa@gmail.com>
2023-07-11 11:19:26 -0700

    fix

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from 'next/navigation';
 import { Description } from '~/components/challenge/description';
 import { getServerAuthSession } from '~/server/auth';
 import { getChallengeRouteData } from './getChallengeRouteData';
+import Comments from '~/components/challenge/comments';
 
 interface Props {
   params: {
@@ -17,5 +18,15 @@ export default async function Challenges({ params: { id } }: Props) {
     return notFound();
   }
 
-  return <Description challenge={challenge} />;
+  return (
+    <div className="relative flex h-full flex-col justify-between">
+      <div className="px-4 py-3">
+        <Description challenge={challenge}></Description>
+      </div>
+
+      <div className="sticky bottom-0 -mx-[1px] overflow-hidden rounded-xl border border-zinc-300 border-b-background bg-background/90 shadow-[0_0_3rem_-0.25rem_#0004] backdrop-blur-sm duration-300 dark:border-zinc-700 dark:border-b-muted dark:bg-muted/90 dark:shadow-[0_0_3rem_-0.25rem_#0008]">
+        <Comments challenge={challenge} />
+      </div>
+    </div>
+  );
 }
--- src/components/challenge/comments/index.tsx
+++ src/components/challenge/comments/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import clsx from 'clsx';
 import { ChevronDown, MessageCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
