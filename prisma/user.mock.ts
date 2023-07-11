c2a17e253e9df15937c83d4d6af59443c695dd16
Jim Burbridge <jhechtf@gmail.com>
2023-07-11 15:30:51 -0700

    feat: Adds in seed script from type-challenge repo

---
+++ prisma/user.mock.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+import { type Prisma, UserStatus } from '@prisma/client';
+
+/**
+ * 
+ * @returns Creates a mock user.
+ */
+export default function UserMock(): Prisma.UserCreateManyInput {
+  // Create email
+  const email = faker.internet.email();
+  // Created At
+  const createdAt = faker.date.between({ from: '2022-06-01', to: new Date() });
+
+  return {
+    email,
+    emailVerified: faker.date.between({ from: createdAt, to: new Date() }),
+    name: faker.person.fullName(),
+    status: faker.helpers.enumValue(UserStatus),
+    createdAt,
+  };
+}
--- src/app/admin/report/page.tsx
+++ src/app/admin/report/page.tsx
@@ -1,11 +1,15 @@
-import { getChallengeReports } from '~/components/admin/admin.actions';
+import { RoleTypes } from '@prisma/client';
+import { getServerAuthSession } from '~/server/auth';
+import { redirect } from 'next/navigation';
 
-export default async function ReportPage() {
-  const reports = await getChallengeReports();
-  return (
-    <div>
-      Reports table here. List all
-      {reports.length}
-    </div>
-  );
+export default async function ListReportPage() {
+  const session = await getServerAuthSession();
+  const roles = session?.user?.role ?? [];
+  const isMod = roles.includes(RoleTypes.ADMIN) || roles.includes(RoleTypes.MODERATOR);
+
+  if (!isMod) {
+    return redirect('/');
+  }
+
+  return <div>I am amazing</div>;
 }
