8978deb9a5236069a1d25a616e4bbc8cece6d135
Cornelius Owusu-Ansah <84820874+thecornisians@users.noreply.github.com>
2023-07-12 03:29:43 +0000

    removed lorem text from explore page (#198)

--- src/components/explore/index.tsx
+++ src/components/explore/index.tsx
@@ -10,16 +10,8 @@ export async function Explore() {
     <div className="container flex h-full flex-col">
       <section className="mb-8 flex max-w-[69ch] flex-col items-start gap-2">
         <h1 className="my-4 bg-gradient-to-r from-[#3178C6] from-30% to-black bg-clip-text text-4xl font-bold tracking-tight text-transparent dark:to-white md:text-6xl lg:my-6">
-          Explore
+          <span className="mb-2 block text-2xl md:text-4xl">Welcome to</span> Type Hero Explore
         </h1>
-        <p className="leading-loose text-neutral-700 dark:text-neutral-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum
-        </p>
       </section>
 
       <Suspense fallback={<ExploreSectionSkeleton />}>
