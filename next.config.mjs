cd04522aa6bb93c7dc6e5bc46e28150e4f391158
Ernesto Resende <dev@ernestoresende.com>
2023-07-12 13:24:17 -0300

    fix 404 pages in vercel deployment (#204)
    * fixed 404 pages in vercel deployment
    
    removing unused i18n config from `next.config.mjs`
    fixes domain prefix assignment on vercel deployments
    
    * fix typo

--- next.config.mjs
+++ next.config.mjs
@@ -4,28 +4,15 @@
  */
 await import('./src/env.mjs');
 await import('./scripts/dl-monaco.mjs').then(({ download }) => download());
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
-
   experimental: {
     serverActions: true,
   },
 
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
 
   eslint: {
