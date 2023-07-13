e404cb6662943f92edd34f832f9a1caa5f82e521
metal-face <hughesbryan3000@gmail.com>
2023-07-13 19:26:47 -0400

    solutions page replicated as a skeleton loader

--- src/components/challenge/solutions/solution-skeleton.tsx
+++ src/components/challenge/solutions/solution-skeleton.tsx
@@ -1,20 +1,29 @@
 export function SolutionsSkeleton() {
   return (
     <div className="flex flex-col space-y-10 px-4 py-3">
-      <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+      {/* SOLUTION BUTTON */}
+      <div className="flex justify-end">
+        <div className="h-7 w-1/6  rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
-      <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+      {/* DIVIDER */}
+      <div className="flex flex-col">
+        <div className="w-6/6 h-1  rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
+      {/* SOLUTIONS */}
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
     </div>
   );
