ce88dd70c7e10fde0c2391125cf511496fa995c5
metal-face <hughesbryan3000@gmail.com>
2023-07-13 19:24:58 -0400

    challenge view replicated as a skeleton

--- src/components/challenge/description/description-skeleton.tsx
+++ src/components/challenge/description/description-skeleton.tsx
@@ -2,19 +2,23 @@ export function DescriptionSkeleton() {
   return (
     <div className="flex flex-col space-y-10 px-4 py-3">
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        {/* TITLE  */}
+        <div className="h-4 w-3/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        {/* BUTTONS */}
+        <div className="h-4 w-3/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
+        {/* DESCRIPTION */}
         <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
         <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 w-1/2 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        {/* FOR EXAMPLE */}
+        <div className="h-4 w-1/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        {/* FOR EXAMPLE BOX */}
+
+        <div className="h-36 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
       </div>
     </div>
   );
