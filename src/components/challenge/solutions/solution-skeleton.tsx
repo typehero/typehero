3855985e8bc6b3c0f2838b76c8ab43ff1df49357
metal-face <hughesbryan3000@gmail.com>
2023-07-13 20:26:01 -0400

    divs upgraded to Skeleton components

--- src/components/challenge/solutions/solution-skeleton.tsx
+++ src/components/challenge/solutions/solution-skeleton.tsx
@@ -1,29 +1,25 @@
+import { Skeleton } from "~/components/ui/skeleton";
+
 export function SolutionsSkeleton() {
   return (
     <div className="flex flex-col space-y-10 px-4 py-3">
       {/* SOLUTION BUTTON */}
       <div className="flex justify-end">
-        <div className="h-7 w-1/6  rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-9 w-1/6  rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
       {/* DIVIDER */}
       <div className="flex flex-col">
-        <div className="w-6/6 h-1  rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="w-6/6 h-1  rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
       {/* SOLUTIONS */}
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
-        <div className="h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="w-6/6 h-10 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
     </div>
   );
