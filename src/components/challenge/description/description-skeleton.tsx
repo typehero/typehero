2f66bc1314d13e4183219c2284881c178f91d50e
metal-face <hughesbryan3000@gmail.com>
2023-07-13 20:33:15 -0400

    divs upgraded to Skeleton components

--- src/components/challenge/description/description-skeleton.tsx
+++ src/components/challenge/description/description-skeleton.tsx
@@ -1,24 +1,25 @@
+import { Skeleton } from '~/components/ui/skeleton';
+
 export function DescriptionSkeleton() {
   return (
     <div className="flex flex-col space-y-10 px-4 py-3">
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
         {/* TITLE  */}
-        <div className="h-4 w-3/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-4 w-2/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
         {/* BUTTONS */}
-        <div className="h-4 w-3/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-4 w-2/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
         {/* DESCRIPTION */}
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
-        <div className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
+        <Skeleton className="h-4 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
       <div className="prose-invert flex flex-col space-y-2 prose-h3:text-xl">
         {/* FOR EXAMPLE */}
-        <div className="h-4 w-1/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-4 w-1/6 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
         {/* FOR EXAMPLE BOX */}
-
-        <div className="h-36 rounded-lg bg-zinc-300 dark:bg-zinc-700"></div>
+        <Skeleton className="h-36 rounded-lg bg-zinc-300 dark:bg-zinc-700"></Skeleton>
       </div>
     </div>
   );
