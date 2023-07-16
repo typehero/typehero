75443fb2a0669ddb6609abfb17bcae66de48f73b
Jim Burbridge <jhechtf@gmail.com>
2023-07-15 21:24:44 -0700

    wip

--- src/app/admin/report/page.tsx
+++ src/app/admin/report/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { RoleTypes } from '@prisma/client';
 import { getServerAuthSession } from '~/server/auth';
 import { redirect } from 'next/navigation';
@@ -12,4 +13,78 @@ export default async function ListReportPage() {
   }
 
   return <div>I am amazing</div>;
+=======
+import { getChallengeReports } from '~/components/admin/admin.actions';
+import React from 'react';
+import { prisma } from '~/server/db';
+import { ReportType } from '@prisma/client';
+import Text from '~/components/ui/typography/typography';
+import { cva } from 'class-variance-authority';
+
+async function getReports() {
+  return prisma.report.findMany({
+    include: {
+      challenge: true,
+      user: true,
+      reporter: true,
+      issues: true
+    },
+    take: 25,
+  });
+}
+
+const f = cva('border p-4 rounded-xl', {
+  variants: {
+    type: {
+      [ReportType.CHALLENGE]: 'border-orange-400',
+      [ReportType.USER]: 'border-green-300',
+      [ReportType.COMMENT]: 'border-yellow-400'
+    }
+  },
+  defaultVariants: {
+    type: 'USER'
+  }
+});
+
+export default async function ReportPage() {
+  const reports = await getReports();
+
+  const bob = reports.reduce((all, cur) => {
+    if(!all[cur.type]) all[cur.type] = [];
+    all[cur.type].push(cur);
+    return all;
+  }, {} as Record<ReportType, (typeof reports[0])[]>);
+  
+  return (
+    <div className="container">
+      {
+        Object.entries(bob)
+          .map(([key, reports]) => {
+            return (
+              <div key={`types-${key}`}>
+                <header className="mb-4">
+                  <Text intent="h1">
+                    {key}
+                  </Text>
+                </header>
+                <section>
+                  {
+                    reports.map(report => (
+                      <div className={f({ type: report.type })} key={`report-${report.id}`}>
+                        Reported By: 
+                        {report.reporter.name} <br />
+                        {report.type === 'CHALLENGE' && report.challenge && report.challenge.name}
+                        <br />User being reported: 
+                        {report.type === 'USER' && report.user && report.user.name}<br />
+                      </div>
+                    ))
+                  }
+                </section>
+              </div>
+            )
+          })
+      }
+    </div>
+  );
+>>>>>>> Stashed changes
 }
