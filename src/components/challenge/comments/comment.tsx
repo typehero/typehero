d2f6a8fa6bcf1aa6971e819c4fafbef407c8f4fb
metal-face <hughesbryan3000@gmail.com>
2023-07-15 20:17:45 -0400

    Upvote downvote functionality removed

--- src/components/challenge/comments/comment.tsx
+++ src/components/challenge/comments/comment.tsx
@@ -188,44 +188,6 @@ const Comment = ({ comment }: CommentProps) => {
                 </Tooltip>
               </div>
               <p>{comment.text}</p>
-              <div className="flex">
-                {/* UPVOTE */}
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="lucide lucide-arrow-big-up stroke-neutral-500 hover:stroke-white"
-                  >
-                    <path d="M9 18v-6H5l7-7 7 7h-4v6H9z" />
-                  </svg>
-                </div>
-                {/* VOTE COUNT */}
-                <div className="text-neutral-500 hover:text-white">0</div>
-                {/* DOWNVOTE */}
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    className="lucide lucide-arrow-big-down stroke-neutral-500 hover:stroke-white"
-                  >
-                    <path d="M15 6v6h4l-7 7-7-7h4V6h6z" />
-                  </svg>
-                </div>
-              </div>
             </div>
 
             <Form {...form}>
