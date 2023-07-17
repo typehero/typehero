787eb0d85c7ab08aa8cf7f8ed0e73619cf8b72fb
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-16 23:17:59 -0400

    explore card tweaks & error fixes

--- src/components/explore/section-skeleton.tsx
+++ src/components/explore/section-skeleton.tsx
@@ -3,11 +3,11 @@ import { ExploreCardSkeleton } from './explore-card-skeleton';
 
 export function ExploreSectionSkeleton() {
   return (
-    <div className="p-5">
-      <div className="flex items-center ">
+    <div className="p-5 container">
+      <div className="flex items-center justify-center">
         <Skeleton className="my-4 h-10 w-1/3 rounded-lg bg-zinc-300 dark:bg-zinc-700" />
       </div>
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid w-full gap-4 sm:grid-cols-2 lg:grid-cols-3">
         <ExploreCardSkeleton />
         <ExploreCardSkeleton />
         <ExploreCardSkeleton />
--- src/utils/relativeTime.ts
+++ src/utils/relativeTime.ts
@@ -1,8 +1,8 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNowStrict } from 'date-fns';
 
 export const getRelativeTime = (date: Date | string) => {
   if (typeof date === 'string') date = new Date(date);
-  return formatDistanceToNow(date, {
+  return formatDistanceToNowStrict(date, {
     addSuffix: true,
   });
 };
