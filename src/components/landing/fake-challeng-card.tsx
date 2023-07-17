b07e16cbf9b9d042053697e67d8e2adc2eac282e
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-17 19:20:14 -0400

    light mode for hero cards

--- src/components/landing/fake-challeng-card.tsx
+++ src/components/landing/fake-challeng-card.tsx
@@ -1,16 +1,15 @@
 const FakeChallengeCard = () => (
   <>
-    <div className="flex flex-col rounded-3xl absolute top-0 right-0 bg-zinc-900 p-8 border dark:border-zinc-600 h-3/4 w-4/5 shadow-[0_0_1rem_#4448]"
-      style={{ boxShadow: 'inset 1rem 1rem 3rem -0.5rem #fff1'}}
+    <div className="flex flex-col rounded-3xl absolute top-0 right-0 bg-background dark:bg-zinc-900 p-8 border dark:border-zinc-600 border-neutral-300 h-3/4 w-4/5 shadow-[0_0_1rem_#8884,1rem_-1rem_3rem_-0.5rem_#0004]"
     >
       <h2 className="text-xl font-bold">Implement a JSON parser type</h2>
-      <div className="rounded-xl -mx-2 bg-zinc-800 flex-grow mt-20"></div>
+      <div className="rounded-xl -mx-2 bg-neutral-200 dark:bg-zinc-800 flex-grow mt-20"></div>
     </div>
-    <div className="flex flex-col rounded-3xl absolute bottom-0 left-0 bg-zinc-900 p-8 border dark:border-zinc-600 h-4/5 w-4/5 shadow-[0_0_1rem_#4448]"
-      style={{ boxShadow: 'inset 1rem 1rem 3rem -0.5rem #fff1, 1rem -1rem 3rem #0008'}}
+    <div className="flex flex-col rounded-3xl absolute bottom-0 left-0 bg-background dark:bg-zinc-900 p-8 border dark:border-zinc-600 border-neutral-300 h-4/5 w-4/5 
+    shadow-[0_0_1rem_#8884,1rem_-1rem_3rem_-0.5rem_#0004]"
     >
       <h2 className="text-xl font-bold">Implement a generic type</h2>
-      <div className="rounded-xl -mx-2 bg-zinc-800 flex-grow mt-20"></div>
+      <div className="rounded-xl -mx-2 bg-neutral-200 dark:bg-zinc-800 flex-grow mt-20"></div>
     </div>
   </>
 )
