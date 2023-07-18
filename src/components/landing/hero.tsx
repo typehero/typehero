b97b744fdd0061f0335ca97ce972907c1b736d84
Ernesto Resende <dev@ernestoresende.com>
2023-07-18 16:53:49 -0300

    shiny smooth animations on the landing cards

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,15 +1,17 @@
-import { Github } from "lucide-react"
+'use client'
+
+import { Github } from 'lucide-react';
+import { motion } from 'framer-motion';
 import { Button } from '~/components/ui/button';
 import { FakeChallengeCard } from '~/components/landing/fake-challenge-card';
 import Link from 'next/link';
-
 
 const TypeHeroLogo = () => {
   return (
     <svg
       fill="none"
       viewBox="0 0 164 164"
-      className="h-28 sm:h-44 w-28 sm:w-44 rounded-3xl sm:rounded-[2rem]"
+      className="h-28 w-28 rounded-3xl sm:h-44 sm:w-44 sm:rounded-[2rem]"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
@@ -30,62 +32,99 @@ const TypeHeroLogo = () => {
   );
 };
 
-const Hero = () => (
-<section className="lg:overflow-x-hidden">
-  {/* <svg className="pointer-events-none mix-blend-soft-light opacity-50 z-10 absolute left-0 top-0 h-full w-full" id="grain">
+const Hero = () => {
+  return (
+    <section className="lg:overflow-x-hidden">
+      {/* <svg className="pointer-events-none mix-blend-soft-light opacity-50 z-10 absolute left-0 top-0 h-full w-full" id="grain">
     <filter id="noise">
       <feTurbulence type="fractalNoise" baseFrequency="1" numOctaves="5" stitchTiles="stitch"></feTurbulence>
     </filter>
     <rect width="100%" height="100%" filter="url(#noise)"></rect>
   </svg> */}
-  <div className="mb-[56px] container md:px-24 items-center justify-center grid lg:grid-cols-2 min-h-[calc(100lvh_-_112px)]">
-    <div className="flex w-full flex-col justify-center items-center lg:items-start gap-10">
-        <div className="relative flex w-full items-center justify-center lg:justify-start gap-4">
-          <div className="absolute left-1/2 top-1/2 -z-10 hidden h-56 w-56 -translate-x-[15%] -translate-y-[50%] rounded-full bg-slate-400/10 blur-3xl dark:block"></div>
-          <div className="absolute right-1/2 top-1/2 -z-10 hidden h-56 w-56 -translate-y-[40%] rounded-full bg-[#3178c6]/20 blur-3xl dark:block"></div>
-          <TypeHeroLogo />
-          <h1 className="bg-gradient-to-r from-[#3178c6] to-black bg-clip-text text-6xl sm:text-8xl font-extrabold sm:leading-[5.5rem] text-transparent dark:to-white">
-            type
-            <br />
-            hero
-          </h1>
+      <div className="container mb-[56px] grid min-h-[calc(100lvh_-_112px)] items-center justify-center md:px-24 lg:grid-cols-2">
+        <div className="flex w-full flex-col items-center justify-center gap-10 lg:items-start">
+          <div className="relative flex w-full items-center justify-center gap-4 lg:justify-start">
+            <div className="absolute left-1/2 top-1/2 -z-10 hidden h-56 w-56 -translate-x-[15%] -translate-y-[50%] rounded-full bg-slate-400/10 blur-3xl dark:block"></div>
+            <div className="absolute right-1/2 top-1/2 -z-10 hidden h-56 w-56 -translate-y-[40%] rounded-full bg-[#3178c6]/20 blur-3xl dark:block"></div>
+            <TypeHeroLogo />
+            <h1 className="bg-gradient-to-r from-[#3178c6] to-black bg-clip-text text-6xl font-extrabold text-transparent dark:to-white sm:text-8xl sm:leading-[5.5rem]">
+              type
+              <br />
+              hero
+            </h1>
+          </div>
+
+          <p className="max-w-[50ch] bg-transparent px-8 text-center leading-9 text-black/50 dark:text-white/50 lg:px-0 lg:text-left">
+            Connect, collaborate, and grow with a community of TypeScript developers. Elevate your
+            skills trough interactive coding challenges, discussions, and knowledge sharing
+          </p>
+          <div className="flex gap-3">
+            <Button className="group relative flex items-center gap-2 overflow-hidden rounded-xl px-4 py-2 font-bold duration-500 dark:hover:text-white">
+              <div className="absolute h-44 w-44 -translate-x-full rounded-full bg-[#3178C6] duration-500 group-hover:translate-x-0" />
+              <Link className="z-10" href="/explore">
+                Explore challenges
+              </Link>
+            </Button>
+            <Button
+              className="flex items-center gap-2 rounded-xl border-2 px-4 py-2 dark:text-white"
+              asChild
+              variant="outline"
+            >
+              <a className="inline-flex gap-1" href="https://github.com/">
+                <Github className="h-4 w-4" />
+                GitHub
+              </a>
+            </Button>
+          </div>
         </div>
-        
-        <p className="max-w-[50ch] bg-transparent leading-9 px-8 lg:px-0 text-center lg:text-left text-black/50 dark:text-white/50">
-          Connect, collaborate, and grow with a community of TypeScript developers. Elevate your
-          skills trough interactive coding challenges, discussions, and knowledge sharing
-        </p>
-        <div className="flex gap-3">
-          <Button className="font-bold relative rounded-xl py-2 px-4 flex items-center gap-2 group dark:hover:text-white duration-500 overflow-hidden">
-            <div className="absolute -translate-x-full h-44 w-44 group-hover:translate-x-0 duration-500 rounded-full bg-[#3178C6]"/>
-            <Link className="z-10" href="/explore">Explore challenges</Link>
-          </Button>
-          <Button
-            className="border-2 rounded-xl py-2 px-4 flex items-center gap-2 dark:text-white"
-            asChild
-            variant="outline"
-          >
-            <a className="inline-flex gap-1" href="https://github.com/">
-              <Github className="h-4 w-4" />
-              GitHub
-            </a>
-          </Button>
+
+        <div className="group relative hidden h-[800px] overflow-visible rounded-full lg:block">
+          <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.3  }}
+          className="moving-grid-background absolute -inset-40 top-1/2 -z-30 aspect-square -translate-y-1/2 translate-x-[-30px] rounded-full shadow-[inset_0_0_5rem_3rem] shadow-background" />
+          <FakeChallengeCard
+            animation={{
+              initial: {
+                opacity: 0,
+                y: 270,
+                x: 20,
+              },
+              animate: {
+                y: 260,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                },
+              },
+            }}
+            className="absolute z-10"
+            title="Implement a JSON parser type"
+          />
+          <FakeChallengeCard
+            animation={{
+              initial: {
+                y: 80,
+                x: 140,
+                opacity: 0,
+              },
+              animate: {
+                y: 70,
+                opacity: 1,
+              },
+              transition: {
+                duration: 0.5,
+                delay: 0.3,
+              }
+            }}
+            difficulty="EASY"
+            className="absolute"
+            title="Implement a generic type"
+          />
         </div>
       </div>
-    
-    <div className="relative h-[800px] group rounded-full lg:block overflow-visible hidden">
-        <div className="moving-grid-background top-1/2 -translate-y-1/2 rounded-full -inset-40 absolute -z-30 aspect-square translate-x-[-30px] shadow-[inset_0_0_5rem_3rem] shadow-background" />
-        <FakeChallengeCard
-          className="absolute group-hover:z-10 group-hover:translate-x-[27px] group-hover:translate-y-[256px] xl:group-hover:translate-x-[77px] translate-x-[100px] xl:translate-x-[190px] translate-y-[116px]"
-          title="Implement a JSON parser type"
-        />
-        <FakeChallengeCard
-          difficulty="EASY"
-          className="absolute group-hover:translate-x-[100px] group-hover:translate-y-[116px] xl:group-hover:translate-x-[190px] translate-x-[27px] xl:translate-x-[77px] translate-y-[256px]"
-          title="Implement a generic type"
-        />
-      </div>
-    </div>
-  </section>
-)
-export default Hero
+    </section>
+  );
+};
+export default Hero;
