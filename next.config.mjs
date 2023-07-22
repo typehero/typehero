94f33ebbb5d5beca65faace690cdd4186df1e230
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-22 11:42:19 -0500

    Convert the dl script to ts

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import('./src/env.mjs');
-await import('./scripts/dl-monaco.mjs').then(({ download }) => download());
+import './src/env.mjs';
 /** @type {import("next").NextConfig} */
 const config = {
   webpack: (config) => {
