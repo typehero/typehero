17fbb3b800ba812794d4b3313574a5a299c36cec
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-07-23 17:53:11 -0500

    Allow only png/jpg

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -4,11 +4,27 @@ const f = createUploadthing();
 
 const auth = (_req: Request) => Promise.resolve({ id: 'fakeId' });
 
+type ValidFileTypes = 'image' | 'video' | 'audio' | 'blob';
+type FileRouterInput =
+  | ValidFileTypes[]
+  | {
+      // @ts-ignore fixed by typehero god
+      [key: ValidFileTypes]: {
+        maxFileSize?: string;
+        maxFileCount?: number;
+      };
+    };
+
+// control the file sizes for all image types
+const DEFAULT_IMAGE_UPLOAD_PARAMS: FileRouterInput = { maxFileSize: '4MB', maxFiles: 1 };
+
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
+  imageUploader: f({
+    'image/png': DEFAULT_IMAGE_UPLOAD_PARAMS,
+    'image/jpeg': DEFAULT_IMAGE_UPLOAD_PARAMS,
+  })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
