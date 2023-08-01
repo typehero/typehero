2a86793d15d3cba1f31c84b34e4089389486f766
Corm <101738478+FullStackCorm@users.noreply.github.com>
2023-08-01 13:26:59 -0400

    Footer date (#309)
    * 2069 was kek but I saw it and was like `yay, an ez thing I can do ᕕ(  ᐛ ) ᕗ` so I did the thing

--- src/components/ui/footsies.tsx
+++ src/components/ui/footsies.tsx
@@ -4,12 +4,12 @@ import { Keyboard as KeyboardIcon } from 'lucide-react';
 export function Footsies() {
   return (
     <footer className="flex flex-col items-center text-sm font-light">
-      <div className="container my-12 flex flex-col justify-between gap-6 px-10 pb-2   lg:flex-row">
+      <div className="container my-12 flex flex-col justify-between gap-6 px-10 pb-2 lg:flex-row">
         <div className="space-x-2">
           Built with <KeyboardIcon className="inline-block h-4 w-4 text-blue-500" /> by the Trash
           Devs community. The source code will soon™️ be available on GitHub
         </div>
-        <div className="text-neutral-500">© 2069 Type Hero</div>
+        <div className="text-neutral-500">© {new Date().getFullYear()} Type Hero</div>
       </div>
     </footer>
   );
