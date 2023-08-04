f725cf1346ff9c4ec7217c269cce994a9a665959
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-04 14:03:27 -0700

    Add og image (#317)
    * Add og image
    
    * Skip auth
    
    * Update image
    
    * Better image
    
    * Better image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,31 @@ import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 import { Navigation } from '~/components/ui/navigation';
 import { Toaster } from '~/components/ui/toaster';
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
-export const metadata = {
-  title: 'Type Hero',
-  description: 'Level up your typescript skills with interactive exercises',
-};
+
+const OG_IMAGE = '/real-og.png';
+export async function generateMetadata(): Promise<Metadata> {
+  const title = 'Type Hero';
+  const description = 'Level up your typescript skills with interactive exercises';
+  return {
+    title,
+    description,
+    openGraph: {
+      images: [OG_IMAGE],
+      description,
+      title,
+      type: 'website',
+    },
+    twitter: {
+      images: [OG_IMAGE],
+      title,
+      description,
+      card: 'summary_large_image',
+    },
+  };
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
