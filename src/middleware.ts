1c86f8940ae06aa6b4d74ee5d30f41cd3bef897e
Chris Bautista <chrisbautista@netflix.com>
2023-08-04 13:39:41 -0700

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
   if (process.env.NODE_ENV === 'production') {
