b53212c407f35d657c24873d2184e0644e773565
Chris Bautista <chrisbautista@netflix.com>
2023-08-07 11:20:01 -0700

    enable  basic auth in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  // don't allow auth on preview domains so we can test shit
-  if (req.headers.get("host") !== "typehero.dev") {
-    return NextResponse.next();
-  }
-
   if (process.env.NODE_ENV === 'production') {
     const basicAuth = req.headers.get('authorization');
     const url = req.nextUrl;
