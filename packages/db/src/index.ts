f52d1066081e57cbdf29e06360ed3d326220bf30
Anthony Shew <anthony.shew@vercel.com>
2023-08-13 00:09:11 -0700

    Prisma so global right now.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ log: ['error', 'warn'] });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
