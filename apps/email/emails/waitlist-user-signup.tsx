832cbc271bb29f1012f53a0e0c2b1493014f177d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-15 19:28:09 -0500

    Center the content

--- apps/email/emails/waitlist-user-signup.tsx
+++ apps/email/emails/waitlist-user-signup.tsx
@@ -1,5 +1,6 @@
 import {
   Body,
+  Button,
   Container,
   Head,
   Heading,
@@ -27,15 +28,9 @@ export const UserSignupEmail = () => {
         <Body className="mx-auto my-auto bg-white font-sans">
           <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
             <Section className="mt-[32px]">
-              <Img
-                src={`${baseUrl}/typehero.png`}
-                width="64"
-                height="auto"
-                alt="Typehero"
-                className="mx-auto my-0"
-              />
+              <Img src={`${baseUrl}/typehero.png`} width="64" height="auto" alt="Typehero" />
             </Section>
-            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-bold text-black">
+            <Heading className="mx-0 my-[30px] p-0 text-[24px] font-bold text-black">
               You're on the Typehero Waitlist!
             </Heading>
             <Text className="pb-2 text-[20px] leading-[24px] text-black">Hey there 👋</Text>
@@ -46,6 +41,16 @@ export const UserSignupEmail = () => {
               We are cooking extremelly hard to bring you the best experience for doing type
               challenges.
             </Text>
+            <Container className="w-fit mx-auto pt-4">
+              <Button
+                pX={20}
+                pY={12}
+                className="rounded bg-[#3178c6] text-center text-[12px] font-semibold text-white no-underline"
+                href="https://twitter.com/typeheroapp"
+              >
+                Follow us on @X
+              </Button>
+            </Container>
           </Container>
         </Body>
       </Tailwind>
