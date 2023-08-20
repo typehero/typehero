1f2f1e391d7429ccf29dbbe2c00cb1016cd0e478
bautistaaa <chrisbautistaaa@gmail.com>
2023-08-20 11:29:41 -0700

    scaffolds admin project (#372)
    * adds admin project
    
    * format

---
+++ apps/admin/global.d.ts
@@ -0,0 +1,15 @@
+declare module '*.md' {
+  const text: string;
+  export default text;
+}
+declare module '*.png';
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    PORT: number;
+    RESEND_API_KEY: string;
+  }
+}
