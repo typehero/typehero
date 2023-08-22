f51c2191a43834602151b1f1bd5dbc241ed55d8a
ggrandi <49359646+ggrandi@users.noreply.github.com>
2023-08-22 23:42:40 +0200

    fully fixed

--- apps/admin/src/app/challenge/[challengeId]/challenge-review.tsx
+++ apps/admin/src/app/challenge/[challengeId]/challenge-review.tsx
@@ -4,9 +4,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useSession } from '@repo/auth/react';
 import { useMemo } from 'react';
-import { Button, DifficultyBadge, Text, useToast, UserBadge } from '@repo/ui';
-import { CodeEditor } from '@repo/monaco/code-editor';
-import { Markdown } from '@repo/ui/markdown';
+import { Button, DifficultyBadge, Text, useToast, UserBadge, Markdown } from '@repo/ui';
 import type { ChallengeToReview } from './page';
 import { approveChallenge, denyChallenge } from './challenge-review.action';
 
@@ -70,7 +68,7 @@ export function ChallengeReview({ challenge }: Props) {
           <Markdown>{challenge.description}</Markdown>
         </div>
         <div className="flex-1 rounded-l-xl rounded-r-2xl border border-zinc-300 dark:border-zinc-700">
-          <CodeEditor value={challenge.tests} />
+          {/* <CodeEditor value={challenge.tests} />   */}
         </div>
       </div>
       <div className="flex justify-end gap-3 p-4">
