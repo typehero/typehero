151c68e2b0593e9a2f25dec623e9efd133785e66
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-22 20:08:51 -0500

    Fix 404 page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-2xl">The page you are looking for is in another castle</p>
+    </div>
+  );
+}
