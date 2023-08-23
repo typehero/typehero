db9b4de24696e906534d88253e4384678db94219
ggrandi <49359646+ggrandi@users.noreply.github.com>
2023-08-23 22:33:33 +0200

    added `K` keymap to vim (#403)
    * added `K` keymap to vim

--- packages/monaco/monaco-vim.d.ts
+++ packages/monaco/monaco-vim.d.ts
@@ -1,13 +1,52 @@
-declare module "monaco-vim" {
-	import type * as monaco from "monaco-editor";
+declare module 'monaco-vim' {
+  import type * as monaco from 'monaco-editor';
 
-	export function initVimMode(
-		editor: monaco.editor.IStandaloneCodeEditor,
-		statusbarNode?: Element | null,
-	): VimMode;
-	
-	class VimMode {
-		/** removes the attached vim bindings */
-		dispose(): void;
-	}
+  export function initVimMode(
+    editor: monaco.editor.IStandaloneCodeEditor,
+    statusbarNode?: Element | null,
+  ): VimMode;
+
+  type VimModes = 'insert' | 'normal' | 'visual';
+
+  export class VimMode {
+    /** removes the attached vim bindings */
+    dispose(): void;
+
+    static Vim: {
+      map(lhs: string, rhs: string, ctx: VimModes): void;
+      unmap(lhs: string, ctx: VimModes): boolean;
+      noremap(lhs: string, rhs: string, ctx: VimModes): void;
+
+      mapCommand(
+        keys: string,
+        type: 'action',
+        name: string,
+        args?: Record<PropertyKey, unknown>,
+        extra?: Record<PropertyKey, unknown>,
+      ): void;
+
+      defineAction(
+        name: string,
+        fn: (
+          cm: { editor: monaco.editor.IStandaloneCodeEditor },
+          arg2: { registerName: null | string; repeat: number; repeatIsExplicit: boolean },
+          arg3: unknown,
+        ) => void,
+      ): void;
+
+      defineEx(
+        name: string,
+        prefix: string | undefined,
+        fn: (
+          ctx: { editor: monaco.editor.IStandaloneCodeEditor },
+          data: {
+            argString: string[];
+            args: string;
+            commandName: string;
+            input: string;
+          } & ({ line: undefined } | { line: number; lineEnd: number }),
+        ) => void,
+      ): void;
+    };
+  }
 }
