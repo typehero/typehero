513ff365556ee95f817770d90209328697a0b904
Anthony Shew <anthony.shew@vercel.com>
2023-08-23 23:16:27 -0700

    Fix root lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@
 module.exports = {
   root: true,
   extends: [require.resolve('./tooling/config-eslint/node')],
+  ignorePatterns: ['apps/**', 'packages/**', 'tooling/**'],
   parserOptions: {
     project: `${__dirname}/tsconfig.json`,
   },
