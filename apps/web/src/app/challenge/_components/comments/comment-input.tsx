a56bb2abeecd08788e9d1af0e730c7e89652bace
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-08-24 07:20:01 -0400

    comments with background container

--- apps/web/src/app/challenge/_components/comments/comment-input.tsx
+++ apps/web/src/app/challenge/_components/comments/comment-input.tsx
@@ -46,7 +46,7 @@ export function CommentInput({ mode, onCancel, onChange, value, placeholder, onS
   };
 
   return (
-    <div className="bg-background/90 flex flex-col rounded-xl rounded-br-lg bg-neutral-100 backdrop-blur-sm dark:border-zinc-700 dark:bg-zinc-700/90">
+    <div className="flex flex-col rounded-xl rounded-br-lg bg-neutral-100 backdrop-blur-sm dark:border-zinc-700 dark:bg-zinc-700/90">
       {commentMode === 'editor' && (
         <Textarea
           autoFocus
