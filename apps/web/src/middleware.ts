aa696e9f17d1addf8f48119c2a039bd3bda5f422
bautistaaa <chrisbautistaaa@gmail.com>
2023-08-25 19:43:11 -0700

    load mocks locally (#426)
    Co-authored-by: Chris Bautista <chrisbautista@netflix.com>

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,9 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
+  if (!process.env.VERCEL_ENV) {
+    return NextResponse.next();
+  }
   const path = req.nextUrl.pathname;
 
   // if path is /explore or /challenge/* and redirect to /waitlist
