efd4c797b33b2e6e61b1c8419041997c9f0603fb
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-08-26 03:30:56 -0400

    og image fancy wow

--- apps/og-image/app/api/default/route.tsx
+++ apps/og-image/app/api/default/route.tsx
@@ -6,17 +6,23 @@ export const runtime = 'edge';
 export async function GET() {
   return new ImageResponse(
     (
-      <div tw="bg-zinc-900 h-full w-full text-white flex flex-col p-14">
-        <div tw="flex flex-col justify-center items-center w-full h-full">
+      <div tw="bg-zinc-900 h-full w-full text-white flex flex-col">
+        <img
+          tw="w-full h-full"
+          src="https://media.discordapp.net/attachments/1124722250294243428/1144691954257510460/image.png?width=2592&height=1397"
+        />
+        {/* <div tw="flex flex-col justify-center items-center w-full h-full">
           <Logo />
           <h1 tw="text-6xl pt-3">Typehero</h1>
-          <p tw="text-center text-3xl text-zinc-300">Level up your typescript skills with interactive exercises</p>
-        </div>
+          <p tw="text-center text-3xl text-zinc-300">
+            Level up your typescript skills with interactive exercises
+          </p>
+        </div> */}
       </div>
     ),
     {
       width: 1200,
-      height: 600,
+      height: 647,
     },
   );
 }
