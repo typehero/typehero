e1e571672b1b3572b4dcc2d4ca03fa55ff7856c2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-08-27 18:02:24 -0500

    Add vercel toolbar to localhost for comments (#412)

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { VercelToolbar } from '@vercel/toolbar/next';
 import { SessionProvider } from '@repo/auth/react';
 import { TooltipProvider } from '@repo/ui';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -15,17 +16,16 @@ interface Props {
 const queryClient = new QueryClient();
 
 export function Providers({ children }: Props) {
+  const shouldInjectToolbar = process.env.NODE_ENV === 'development';
+
   return (
-    <BalancerProvider>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider>
-          <ThemeProvider attribute="class">
-            <FeatureFlagProvider>
-              <TooltipProvider>{children}</TooltipProvider>
-            </FeatureFlagProvider>
-          </ThemeProvider>
-        </SessionProvider>
-      </QueryClientProvider>
-    </BalancerProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <ThemeProvider attribute="class">
+          <TooltipProvider>{children}</TooltipProvider>
+          {shouldInjectToolbar ? <VercelToolbar /> : null}
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
