92af432c333fed0395a4f666956ce411d263ae54
bautistaaa <chrisbautistaaa@gmail.com>
2023-08-28 22:06:51 -0700

    fix: middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,10 +1,9 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
-const STAGING_DOMAIN = 'staging.typehero.dev';
+const STAGING_DOMAIN = 'web-staging';
 export function middleware(req: NextRequest) {
-  console.log({ VERCEL_ENV: process.env.VERCEL_ENV, VERCEL_URL: process.env.VERCEL_URL });
   // skip blocking the request if we are on staging or local
-  if (!process.env.VERCEL_ENV || process.env.VERCEL_URL === STAGING_DOMAIN) {
+  if (!process.env.VERCEL_ENV || process.env.VERCEL_URL?.includes(STAGING_DOMAIN)) {
     return NextResponse.next();
   }
 
