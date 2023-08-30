4e3da89ab35f0692d4b618dceea0754b1e0a58e6
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-08-30 19:43:21 -0400

    profile pretti ✨

--- packages/ui/src/components/tabs.tsx
+++ packages/ui/src/components/tabs.tsx
@@ -50,7 +50,7 @@ const VerticalTabsTrigger = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Trigger
     className={cn(
-      'border-border dark:border-ring border data-[state=active]:bg-border duration-300 focus-visible:ring-offset-2 rounded-3xl ring-offset-background focus-visible:ring-ring data-[state=active]:text-foreground whitespace-nowrap px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm',
+      'border-border dark:border-ring data-[state=active]:bg-border ring-offset-background focus-visible:ring-ring data-[state=active]:text-foreground whitespace-nowrap rounded-xl border px-3 py-1.5 text-sm font-medium transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm',
       className,
     )}
     ref={ref}
