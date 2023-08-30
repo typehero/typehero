fa2dab32e14c2410e4c1e7c8628882b0fc4e5f8e
jproberson <50461518+jproberson@users.noreply.github.com>
2023-08-30 14:49:11 -0500

    Fullscreen toggle (#515)
    * fullscreen toggle
    
    * cleanup
    
    * combined isFullScreen into settings, easy to separate if wanted
    
    * prettier
    
    * settings
    
    * conditionally render resizer for fullscreen toggle

--- apps/web/src/app/challenge/_components/fullscreen.tsx
+++ apps/web/src/app/challenge/_components/fullscreen.tsx
@@ -1,11 +1,21 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from '@repo/ui';
 import { Maximize2 } from '@repo/ui/icons';
+import { useLayoutSettingsStore } from './challenge-layout';
 
 export function FullscreenButton() {
+  const { settings, updateSettings } = useLayoutSettingsStore();
+
+  const handleToggleFullscreen = () => {
+    updateSettings({ ...settings, isFullscreen: !settings.isFullscreen });
+  };
+
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <button className="focus:outline-none focus-visible:ring-2">
+        <button
+          className="focus:outline-none focus-visible:ring-2"
+          onClick={handleToggleFullscreen}
+        >
           <Maximize2 className="stroke-zinc-500 stroke-1 hover:stroke-zinc-400" size={20} />
         </button>
       </TooltipTrigger>
