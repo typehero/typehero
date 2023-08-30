dfc7a59b3478214f8110e2c0cac1adaad08df7e4
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-08-30 11:40:48 +0530

    chore: random change (test pr).

--- apps/web/src/app/challenge/error.tsx
+++ apps/web/src/app/challenge/error.tsx
@@ -1,13 +1,13 @@
 'use client'; // Error components must be Client Components
 
-import Link from 'next/link';
 import { Button, Text } from '@repo/ui';
+import Link from 'next/link';
 
 export default function Error() {
   return (
     <div className="container flex h-full flex-col items-center justify-center">
       <Text className="mb-6" intent="h2">
-        Uh oh! We couldn&apos;t find the challenge you were looking for.
+        Welp! We couldn&apos;t find the challenge you were looking for. :3
       </Text>
       <Link href="/explore">
         <Button>Explore Challenges</Button>
