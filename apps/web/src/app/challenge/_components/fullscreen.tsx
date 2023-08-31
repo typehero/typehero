725ed4bec31a83b3306297faa573be880aca9f4f
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-08-30 20:07:32 -0400

    fullscreen button workee wooo fullscreening i lov fullscreening woooo… (#524)
    * fullscreen button workee wooo fullscreening i lov fullscreening woooooooo
    
    * useEffect

--- apps/web/src/app/challenge/_components/fullscreen.tsx
+++ apps/web/src/app/challenge/_components/fullscreen.tsx
@@ -1,13 +1,46 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from '@repo/ui';
 import { Maximize2 } from '@repo/ui/icons';
-import { useLayoutSettingsStore } from './challenge-layout';
+import { Minimize2 } from 'lucide-react';
+import { useEffect } from 'react';
+import { create } from 'zustand';
+
+export const FS_SETTINGS = {
+  isFullscreen: false,
+};
+
+type FSSettings = typeof FS_SETTINGS;
+
+interface FSState {
+  fssettings: FSSettings;
+  updateFSSettings: (settings: Partial<FSSettings>) => void;
+}
+
+export const useFullscreenSettingsStore = create<FSState>()((set, get) => ({
+  fssettings: FS_SETTINGS,
+  updateFSSettings: (fssettings) => set({ fssettings: { ...get().fssettings, ...fssettings } }),
+}));
 
 export function FullscreenButton() {
-  const { settings, updateSettings } = useLayoutSettingsStore();
+  const { fssettings, updateFSSettings } = useFullscreenSettingsStore();
 
   const handleToggleFullscreen = () => {
-    updateSettings({ ...settings, isFullscreen: !settings.isFullscreen });
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+    updateFSSettings({ ...fssettings, isFullscreen: !fssettings.isFullscreen });
   };
+
+  useEffect(() => {
+    const fullscreenchanged = () => {
+      if (!document.fullscreenElement) {
+        updateFSSettings({ ...fssettings, isFullscreen: false });
+      }
+    };
+    document.addEventListener('fullscreenchange', fullscreenchanged);
+    return () => document.removeEventListener('fullscreenchange', fullscreenchanged);
+  }, [fssettings, updateFSSettings]);
 
   return (
     <Tooltip>
@@ -16,7 +49,11 @@ export function FullscreenButton() {
           className="focus:outline-none focus-visible:ring-2"
           onClick={handleToggleFullscreen}
         >
-          <Maximize2 className="stroke-zinc-500 stroke-1 hover:stroke-zinc-400" size={20} />
+          {fssettings.isFullscreen ? (
+            <Minimize2 className="stroke-zinc-500 stroke-1 hover:stroke-zinc-400" size={20} />
+          ) : (
+            <Maximize2 className="stroke-zinc-500 stroke-1 hover:stroke-zinc-400" size={20} />
+          )}
         </button>
       </TooltipTrigger>
       <TooltipContent>
