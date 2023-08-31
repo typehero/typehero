9bddf2f7f5ab4e0d33bfe5c118c22d9579b7177a
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-08-30 20:29:02 -0400

    Profile 2 (#526)
    * rm un-used classname, add more gap
    
    * hide vertical nav on small screens
    
    * add placeholder for sheet
    
    * add shadcn sheet ui component
    
    * rm holy sheet
    
    * start adding icons to vertical tabs
    
    * start cooking horizontally
    
    * update gaps
    
    * profile pretti ✨
    
    * add py for bottom padding
    
    * fix a little typo
    
    ---------
    
    Co-authored-by: maxdemaio <maxdman321@gmail.com>

--- packages/ui/src/components/magic-icon.tsx
+++ packages/ui/src/components/magic-icon.tsx
@@ -3,12 +3,13 @@ import { Github, Link as LinkIcon, Linkedin, Twitter, Youtube } from 'lucide-rea
 // TODO: this could be more robust
 // it's not accounting for subdomains like the boomer www and stuff
 export function MagicIcon({ url }: { url: string }) {
-  if (url.startsWith('https://github.com/')) return <Github className="h-5 w-5 text-neutral-400" />;
+  if (url.startsWith('https://github.com/'))
+    return <Github className="h-3 w-3 text-neutral-400 dark:text-neutral-600" />;
   if (url.startsWith('https://twitter.com/'))
-    return <Twitter className="h-5 w-5 text-neutral-400" />;
+    return <Twitter className="h-3 w-3 text-neutral-400 dark:text-neutral-600" />;
   if (url.startsWith('https://www.linkedin.com/'))
-    return <Linkedin className="h-5 w-5 text-neutral-400" />;
+    return <Linkedin className="h-3 w-3 text-neutral-400 dark:text-neutral-600" />;
   if (url.startsWith('https://youtube.com/'))
-    return <Youtube className="h-5 w-5 text-neutral-400" />;
-  return <LinkIcon className="h-5 w-5 text-neutral-400" />;
+    return <Youtube className="h-3 w-3 text-neutral-400 dark:text-neutral-600" />;
+  return <LinkIcon className="h-3 w-3 text-neutral-400 dark:text-neutral-600" />;
 }
