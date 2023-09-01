321f98ce631013221edc62dbd767e479207236ff
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-08-31 22:15:43 -0400

    lint fix

--- apps/web/src/components/landing/features.tsx
+++ apps/web/src/components/landing/features.tsx
@@ -33,7 +33,7 @@ function Features() {
               className="mx-auto hidden rounded-full bg-gradient-to-r from-[#31bdc6] to-[#3178c6] p-[1px] brightness-90 contrast-150 focus:outline-none focus:ring-blue-600 focus-visible:ring-2 dark:brightness-125 dark:contrast-100 sm:block"
               href="#features"
             >
-              <div className="rounded-full group relative duration-300 hover:pr-9 overflow-hidden bg-white/80 px-3 py-1 dark:bg-black/80">
+              <div className="group relative overflow-hidden rounded-full bg-white/80 px-3 py-1 duration-300 hover:pr-9 dark:bg-black/80">
                 <span className="bg-gradient-to-r from-[#31bdc6] to-[#3178c6] bg-clip-text text-transparent">
                   <svg
                     className="mr-1 inline-block h-4 w-4 fill-[#31bdc6]"
@@ -42,7 +42,14 @@ function Features() {
                   >
                     <path d="m19.2 36.4-4.75-10.45L4 21.2l10.45-4.75L19.2 6l4.75 10.45L34.4 21.2l-10.45 4.75ZM36.4 42l-2.35-5.25-5.25-2.35 5.25-2.4 2.35-5.2 2.4 5.2 5.2 2.4-5.2 2.35Z" />
                   </svg>
-                  many features, wow <Image className="absolute duration-300 translate-y-7 group-hover:translate-y-0 -bottom-1 right-1" alt="doge smile" height="28" width="28" src={DogeSmile} />
+                  many features, wow{' '}
+                  <Image
+                    className="absolute -bottom-1 right-1 translate-y-7 duration-300 group-hover:translate-y-0"
+                    alt="doge smile"
+                    height="28"
+                    width="28"
+                    src={DogeSmile}
+                  />
                 </span>
               </div>
             </a>
