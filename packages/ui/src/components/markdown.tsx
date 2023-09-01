059fcc5fa93436b23212895d55ba6cfa27da5023
Aodhan Hamilton <hamilton.aodhan@gmail.com>
2023-09-01 23:58:54 +1200

    border (#531)
    * border
    
    * pnpm checks:fix ran
    
    * delete packages/email/yarn.lock
    
    * hopefully i fixed it
    
    * Update packages/ui/src/components/markdown.tsx
    
    * Update packages/ui/src/components/markdown.tsx
    
    Co-authored-by: Sean Boult <996134+Hacksore@users.noreply.github.com>
    
    * pnpm checks:fix
    
    * pnpm checks:fix again
    
    * delete yarn.lock
    
    ---------
    
    Co-authored-by: bautistaaa <chrisbautistaaa@gmail.com>
    Co-authored-by: Sean Boult <996134+Hacksore@users.noreply.github.com>

--- packages/ui/src/components/markdown.tsx
+++ packages/ui/src/components/markdown.tsx
@@ -73,7 +73,7 @@ export function Markdown({ children, className }: { children: string; className?
               {String(children).replace(/\n$/, '')}
             </SyntaxHighlighter>
           ) : (
-            <code className="rounded-md bg-neutral-200 p-1 font-mono dark:bg-black">
+            <code className="rounded-md border border-zinc-300 bg-neutral-200 p-1 font-mono text-zinc-600 dark:border-zinc-600 dark:bg-zinc-700 dark:text-zinc-300">
               {children}
             </code>
           );
