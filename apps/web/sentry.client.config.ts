7dc450ec7caf9531bd0ff73a9ce66041f5996865
bautistaaa <chrisbautistaaa@gmail.com>
2023-09-01 07:30:42 -0700

    Update apps/web/sentry.client.config.ts
    Co-authored-by: Luca Forstner <luca.forstner@gmail.com>

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -9,6 +9,9 @@ Sentry.init({
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
+  
+  // Request targets to which we want Sentry tracing headers attached
+  tracePropagationTargets: ["localhost:3000", /^\//],
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
