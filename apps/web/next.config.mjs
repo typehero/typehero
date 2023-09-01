00940d5b13d5352cc651252348abb162103053bb
bautistaaa <chrisbautistaaa@gmail.com>
2023-09-01 13:15:07 -0700

    chore: adds sentry (#547)
    * chore: adds sentry
    
    * Update apps/web/sentry.client.config.ts
    
    Co-authored-by: Luca Forstner <luca.forstner@gmail.com>
    
    * formatting
    
    ---------
    
    Co-authored-by: Luca Forstner <luca.forstner@gmail.com>

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 // NOTE: this whole package is bugged and once they fix this we can remove this workaround
 // eslint-disable-next-line import/no-unresolved
 import vercelToolbar from '@vercel/toolbar/plugins/next';
+import { withSentryConfig } from '@sentry/nextjs';
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import("next").NextConfig} */
@@ -41,7 +42,37 @@ const nextConfig = {
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
-
 const withVercelToolbar = vercelToolbar();
 
-export default withBundleAnalyzer(withVercelToolbar(nextConfig));
+export default withSentryConfig(
+  withBundleAnalyzer(withVercelToolbar(nextConfig)),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: 'typehero',
+    project: 'typehero-web-production',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  },
+);
