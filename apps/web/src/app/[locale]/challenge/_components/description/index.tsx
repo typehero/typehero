f01345fda1c88454de3042d450d4680bca0be38d
bautistaaa <chrisbautistaaa@gmail.com>
2023-09-05 16:18:15 -0700

    remove invalid html structure

--- apps/web/src/app/[locale]/challenge/_components/description/index.tsx
+++ apps/web/src/app/[locale]/challenge/_components/description/index.tsx
@@ -88,15 +88,10 @@ export function Description({ challenge }: Props) {
       <div className="mt-3 flex items-center gap-3">
         <DifficultyBadge difficulty={challenge.difficulty} />
         <Dialog>
-          <DialogTrigger tabIndex={-1}>
+          <DialogTrigger>
             <Tooltip>
               <TooltipTrigger asChild>
-                <button className="group flex h-6 items-center rounded-full bg-zinc-200 px-3 focus:outline-none focus-visible:ring-2 dark:bg-zinc-700">
-                  <Share
-                    className="h-4 w-4 stroke-zinc-500 group-hover:stroke-zinc-600 dark:stroke-zinc-300 group-hover:dark:stroke-zinc-100"
-                    tabIndex={-1}
-                  />
-                </button>
+                <Share className="h-4 w-4 stroke-zinc-500 group-hover:stroke-zinc-600 dark:stroke-zinc-300 group-hover:dark:stroke-zinc-100" />
               </TooltipTrigger>
               <TooltipContent>
                 <p>Share challenge</p>
