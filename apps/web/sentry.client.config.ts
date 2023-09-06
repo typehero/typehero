bd402057ce9e97d41c42450013b6e6dfb1567252
QuiiBz <tom.lienrd@gmail.com>
2023-09-06 08:55:13 +0200

    feat(web): remove sentry replay

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -16,24 +16,9 @@ Sentry.init({
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
   enabled: process.env.NODE_ENV === 'production',
   environment:
     window.origin.includes('staging') || window.origin.includes('vercel.app')
       ? 'Staging'
       : 'Production',
-
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    new Sentry.Replay({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
 });
