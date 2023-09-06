06179922fcdd647e5d426d8c4d122cd19a80824a
QuiiBz <tom.lienrd@gmail.com>
2023-09-06 12:26:13 +0200

    fix: format

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -1,9 +1,9 @@
 import { getStaticParams } from '~/locales/server';
 
 export function generateStaticParams() {
-  return getStaticParams()
+  return getStaticParams();
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children
+  return children;
 }
