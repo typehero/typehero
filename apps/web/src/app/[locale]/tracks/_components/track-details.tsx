f65914160a08c6162e3f6d42d331e7971c19f210
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-09-06 21:58:15 +0200

    change challenge sorting to orderId instead of difficulty

--- apps/web/src/app/[locale]/tracks/_components/track-details.tsx
+++ apps/web/src/app/[locale]/tracks/_components/track-details.tsx
@@ -4,37 +4,23 @@ import Link from 'next/link';
 import { EnrollButton } from './enroll-button';
 import { TrackChallenge } from './track-challenge-card';
 import { TrackProgress } from './track-progress';
-import {
-  enrollUserInTrack,
-  getTrackDetails,
-  unenrollUserFromTrack,
-} from './track.action';
+import { enrollUserInTrack, getTrackDetails, unenrollUserFromTrack } from './track.action';
 
 interface TrackDetailProps {
   // trackid
   slug: string;
 }
 
-const difficultyToNumber = {
-  BEGINNER: 0,
-  EASY: 1,
-  MEDIUM: 2,
-  HARD: 3,
-  EXTREME: 4,
-} as const;
-
 export async function TrackDetail({ slug }: TrackDetailProps) {
   const session = await getServerAuthSession();
   const trackId = parseInt(slug);
   const track = await getTrackDetails(trackId);
-  const challenges = track?.trackChallenges.map(x => x.challenge) ?? []
-  const isEnrolled = track?.enrolledUsers.findIndex(
-    (user) => user.id === session?.user.id,
-  );
+  const trackChallenges = track?.trackChallenges ?? [];
+  const isEnrolled = track?.enrolledUsers.findIndex((user) => user.id === session?.user.id);
   // Calculates the total number of successful challenges.
   function calulcateCompletedChallenges(): number {
     let completedChallenges = 0;
-    for (const trackChallenge of track!.trackChallenges) {
+    for (const trackChallenge of trackChallenges) {
       for (const submission of trackChallenge.challenge.submission) {
         if (submission.isSuccessful) {
           completedChallenges++;
@@ -56,29 +42,37 @@ export async function TrackDetail({ slug }: TrackDetailProps) {
         <p className=" max-w-[69ch] text-lg leading-10 text-neutral-600 dark:text-white/50">
           {track?.description}
         </p>
-        {isEnrolled !== -1 && <div className="my-3">
-          <TrackProgress
-            completedChallenges={calulcateCompletedChallenges()}
-            totalChallenges={challenges.length}
-          />
-        </div>}
+        {isEnrolled !== -1 && (
+          <div className="my-3">
+            <TrackProgress
+              completedChallenges={calulcateCompletedChallenges()}
+              totalChallenges={trackChallenges.length}
+            />
+          </div>
+        )}
         {isEnrolled === -1 ? (
           <EnrollButton action={enrollUserInTrack} trackId={trackId} text="Enroll" />
         ) : (
           <EnrollButton action={unenrollUserFromTrack} trackId={trackId} text="Unenroll" />
         )}
       </div>
-      <div className="flex flex-col min-w-[50%] max-w-xl">
-        {challenges
-            .sort((a, b) =>
-              difficultyToNumber[a.difficulty] !== difficultyToNumber[b.difficulty]
-                ? difficultyToNumber[a.difficulty] - difficultyToNumber[b.difficulty]
-                : a.name.localeCompare(b.name),
-            )
-            .map((challenge) => (
-              <Link  key={challenge.id} href={`/challenge/${challenge.id}`}>
-                <TrackChallenge challenge={challenge} challengeInProgress={challenge.submission.length > 0 && !challenge.submission.some(submission => submission.isSuccessful)} mock showDescriptionOnHover/></Link>
-            ))}
+      <div className="flex min-w-[50%] max-w-xl flex-col">
+        {trackChallenges
+          .sort((a, b) => a.orderId - b.orderId)
+          .map((x) => x.challenge)
+          .map((challenge) => (
+            <Link key={challenge.id} href={`/challenge/${challenge.id}`}>
+              <TrackChallenge
+                challenge={challenge}
+                challengeInProgress={
+                  challenge.submission.length > 0 &&
+                  !challenge.submission.some((submission) => submission.isSuccessful)
+                }
+                mock
+                showDescriptionOnHover
+              />
+            </Link>
+          ))}
       </div>
     </div>
   );
