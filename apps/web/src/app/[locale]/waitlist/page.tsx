02a33ae3104b1c7e701cf85761045c235f6ed58b
QuiiBz <tom.lienrd@gmail.com>
2023-09-08 17:16:47 +0200

    fix: revert waitlist

--- apps/web/src/app/[locale]/waitlist/page.tsx
+++ apps/web/src/app/[locale]/waitlist/page.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from 'next';
-import { getStaticParams } from '~/locales/server';
 
 export const metadata: Metadata = {
   title: 'Waitlist | TypeHero',
   description: 'Join the waitlist for TypeHero and be the first to know when we launch!',
 };
-
-export function generateStaticParams() {
-  return getStaticParams();
-}
 
 export { Waitlist as default } from './_components';
