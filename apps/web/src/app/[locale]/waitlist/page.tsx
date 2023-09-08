c579572d02b05c5c0a9d973de8ad521155b70770
QuiiBz <tom.lienrd@gmail.com>
2023-09-08 17:05:13 +0200

    feat(web): static rendering on `/` and `/waitlist`

--- apps/web/src/app/[locale]/waitlist/page.tsx
+++ apps/web/src/app/[locale]/waitlist/page.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next';
-
-export { Waitlist as default } from './_components';
+import { getStaticParams } from '~/locales/server';
 
 export const metadata: Metadata = {
   title: 'Waitlist | TypeHero',
   description: 'Join the waitlist for TypeHero and be the first to know when we launch!',
 };
+
+export function generateStaticParams() {
+  return getStaticParams();
+}
+
+export { Waitlist as default } from './_components';
