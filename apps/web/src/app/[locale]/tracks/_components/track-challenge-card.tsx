73dacd5d90c8a5612376eddc75114750812a48f2
aakash19here <spacex3nasa@gmail.com>
2023-09-09 21:47:25 +0530

    null check added

--- apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
+++ apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
@@ -33,19 +33,16 @@ const BGS_BY_DIFFICULTY = {
 } as const;
 
 const MockComponent = ({ challenge, mock }: MockComponentProps) => {
-  if (mock) {
-    return (
-      <>
-        <input
-          className="peer hidden appearance-none"
-          type="checkbox"
-          id={challenge.id.toString()}
-        />
-        <div className="h-5 w-5 rounded-full border border-black/70 bg-black/10 duration-75 peer-checked:border-transparent peer-checked:bg-green-600/80 dark:border-white/50 dark:bg-white/10 peer-checked:dark:bg-green-300/80" />
-        <Check className="absolute left-1 my-auto h-3 w-3 scale-0 stroke-[4] text-white duration-300 peer-checked:scale-100 dark:text-black" />
-      </>
-    );
+  if (!mock) {
+    return null;
   }
+  return (
+    <>
+      <input className="peer hidden appearance-none" type="checkbox" id={challenge.id.toString()} />
+      <div className="h-5 w-5 rounded-full border border-black/70 bg-black/10 duration-75 peer-checked:border-transparent peer-checked:bg-green-600/80 dark:border-white/50 dark:bg-white/10 peer-checked:dark:bg-green-300/80" />
+      <Check className="absolute left-1 my-auto h-3 w-3 scale-0 stroke-[4] text-white duration-300 peer-checked:scale-100 dark:text-black" />
+    </>
+  );
 };
 
 export function TrackChallenge({ challenge, className }: TrackChallengeProps) {
