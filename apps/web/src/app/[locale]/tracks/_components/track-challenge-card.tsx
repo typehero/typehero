63d638a1643a8e3729fd8e3232791a7cfc684e5f
laureanray <laureanraybahala@gmail.com>
2023-09-10 23:05:13 +0800

    make track challenge interactive using keyboard

--- apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
+++ apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
@@ -4,6 +4,7 @@ import { Check } from '@repo/ui/icons';
 import clsx from 'clsx';
 import { useIsMobile } from '~/utils/useIsMobile';
 import type { Tracks } from './track-grid';
+import { useEffect, useRef, useState } from 'react';
 
 interface TrackChallengeProps {
   challenge: Tracks[number]['trackChallenges'][number]['challenge'];
@@ -36,11 +37,12 @@ const MockComponent = ({ challenge, mock }: MockComponentProps) => {
   if (!mock) {
     return null;
   }
+
   return (
     <>
       <input className="peer hidden appearance-none" type="checkbox" id={challenge.id.toString()} />
       <div className="h-5 w-5 rounded-full border border-black/70 bg-black/10 duration-75 peer-checked:border-transparent peer-checked:bg-green-600/80 dark:border-white/50 dark:bg-white/10 peer-checked:dark:bg-green-300/80" />
-      <Check className="absolute left-1 my-auto h-3 w-3 scale-0 stroke-[4] text-white duration-300 peer-checked:scale-100 dark:text-black" tabIndex={0}/>
+      <Check className="absolute left-1 my-auto h-3 w-3 scale-0 stroke-[4] text-white duration-300 peer-checked:scale-100 dark:text-black" />
     </>
   );
 };
@@ -48,21 +50,34 @@ const MockComponent = ({ challenge, mock }: MockComponentProps) => {
 export function TrackChallenge({ challenge, className }: TrackChallengeProps) {
   const isMobile = useIsMobile();
 
+
   return (
     <label
       htmlFor={challenge.id.toString()}
-      className="group/challenge flex cursor-pointer flex-col items-center pt-2"
+      className="group/challenge flex cursor-pointer flex-col items-center pt-2 focus:outline-none"
+      tabIndex="0"
+      onKeyDown={(e) => {
+        const clickEvent = new MouseEvent("click", {
+          "view": window,
+          "bubbles": true,
+          "cancelable": false
+        });
+        if (e.key === 'Enter') {
+          e.target.dispatchEvent(clickEvent);
+        }
+      }
+      }
     >
       <div
         className={clsx(
           `flex w-full items-center justify-between gap-3 overflow-hidden rounded-lg`,
-          `bg-gradient-to-r from-neutral-500/10 from-70% ${
-            BGS_BY_DIFFICULTY[challenge.difficulty]
+          `bg-gradient-to-r from-neutral-500/10 from-70% ${BGS_BY_DIFFICULTY[challenge.difficulty]
           } to-100% dark:from-neutral-500/20`,
           ` p-4 py-2 text-black/90 duration-300 group-active/challenge:bg-neutral-500/40 group-active/challenge:duration-75 dark:text-white/90 sm:py-4`,
           className,
           !isMobile &&
-            'group-hover/challenge:scale-105 group-hover/challenge:rounded-xl group-hover/challenge:bg-neutral-500/20',
+          'group-hover/challenge:scale-105 group-hover/challenge:rounded-xl group-hover/challenge:bg-neutral-500/20',
+          'group-focus/challenge:outline-none group-focus-visible/challenge:ring-2 group-focus-visible/challenge:ring-blue-600'
         )}
       >
         <div className="relative flex items-center gap-3 text-xs sm:text-base">
@@ -70,14 +85,12 @@ export function TrackChallenge({ challenge, className }: TrackChallengeProps) {
           {challenge.name}
         </div>
         <div
-          className={`relative text-xs font-medium tracking-wide ${
-            COLORS_BY_DIFFICULTY[challenge.difficulty]
-          } text-background`}
+          className={`relative text-xs font-medium tracking-wide ${COLORS_BY_DIFFICULTY[challenge.difficulty]
+            } text-background`}
         >
           <div
-            className={`absolute right-0 top-1/2 h-12 w-12 -translate-y-1/2 blur-3xl ${
-              BGS_BY_DIFFICULTY[challenge.difficulty]
-            }`}
+            className={`absolute right-0 top-1/2 h-12 w-12 -translate-y-1/2 blur-3xl ${BGS_BY_DIFFICULTY[challenge.difficulty]
+              }`}
           />
           {challenge.difficulty[0]}
           {challenge.difficulty.substring(1, challenge.difficulty.length).toLowerCase()}
