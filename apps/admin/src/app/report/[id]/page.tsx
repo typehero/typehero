b2a4dcf589ce4e42884a8dfba26c3a1056b79934
Anurag Singh <sa.anuragsingh@gmail.com>
2023-09-19 23:17:44 +0530

    fix: alignment issues in action already taked alert

--- apps/admin/src/app/report/[id]/page.tsx
+++ apps/admin/src/app/report/[id]/page.tsx
@@ -71,18 +71,18 @@ const Report = async function (props: Props) {
         <ReportActions report={report} />
       </div>
 
-      {report.moderator ? (
+      {report.moderator && (
         <Alert className="mt-4" variant="destructive">
-          <div className="flex items-center gap-2">
+          <div className="mb-0.5 flex items-center gap-2">
             <AlertCircle className="h-[1.25rem]" />
-            <AlertTitle>Action already taken</AlertTitle>
+            <AlertTitle className="mb-0">Action already taken</AlertTitle>
           </div>
 
           <AlertDescription>
             Moderator @{report.moderator.name} took action on this case already.
           </AlertDescription>
         </Alert>
-      ) : null}
+      )}
 
       <div className="wrapper mt-4 flex flex-col gap-4 md:flex-row">
         <section className="order-2 flex-grow md:order-1">{ReportEl(report)}</section>
