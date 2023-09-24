04808b7e7afdf57b0eeb5e281ebf3c1e091cdf62
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-24 14:11:14 -0500

    Update robots.txt

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -4,8 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/api/og/*',
-      disallow: '/private/',
+      disallow: ['/api/*'],
     },
     sitemap: '/sitemap.xml',
   };
