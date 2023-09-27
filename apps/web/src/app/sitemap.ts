0446d28a753b01de069dbcaf59669d47828985c8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-09-27 06:36:55 -0500

    Better

---
+++ apps/web/src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+const URL = 'https://typehero.dev';
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${URL}/`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${URL}/challenge`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${URL}/tracks`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ];
+}
