6983047d10ac0f18bc475896a0b9470bd96a50fe
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-09-27 21:37:34 -0400

    this is perfect

--- apps/og-image/app/api/challenge/route.tsx
+++ apps/og-image/app/api/challenge/route.tsx
@@ -1,7 +1,8 @@
 import { ImageResponse } from '@vercel/og';
 import { challengeParam } from 'utils/zodParams';
 import { fetchFont } from 'utils/fetchFont';
-import { Logo } from 'app/components/logo';
+import { Logo3d } from 'app/components/logo3d';
+import { Grid } from 'app/components/grid';
 
 export const runtime = 'edge';
 
@@ -20,27 +21,74 @@ export async function GET(req: Request) {
 
   const props = parsed.data.input;
 
+  const COLORS_BY_DIFFICULTY = {
+    BEGINNER: 'bg-[#86cae9]',
+    EASY: 'bg-[#85efac]',
+    MEDIUM: 'bg-[#f19c41]',
+    HARD: 'bg-[#f87272]',
+    EXTREME: 'bg-[#d8b4fe]',
+  };
+
+  const HOST =
+    process.env.NODE_ENV === 'production' ? 'https://og.typehero.dev' : 'http://localhost:4200';
+
   return new ImageResponse(
     (
-      <div tw="bg-black h-full w-full text-white bg-cover flex flex-col pt-14 px-24">
-        <div tw="flex flex-col items-start h-full px-10 py-4 rounded-t-3xl border border-b-none border-zinc-700 bg-zinc-800">
-          <Logo />
-          <h1 tw="text-6xl font-bold truncate" style={{ fontWeight: '800 !important' }}>
-            {props.title}
-          </h1>
-          <p tw="flex items-center">
-            <p tw="text-3xl font-bold bg-zinc-600 rounded-full px-3 py-1.5">
-              <span>@</span>
-              <span>{props.username}</span>
+      <div tw="bg-black h-full w-full text-white bg-cover flex flex-col pt-14 px-14">
+        <Grid />
+        <img src={`${HOST}/amoguwuowoaahhh.png?cache-bust=${new Date().getDate()}`} alt="OG" />
+        <div tw="flex flex-col items-start h-full overflow-hidden rounded-t-[3.5rem] border-zinc-700 border-2 border-b-0 to-black relative">
+          <svg
+            // @ts-ignore
+            tw="absolute h-full w-full"
+            viewBox="0 0 512 512"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="512" height="512" fill="url(#paint0_linear_970_280)" />
+            <defs>
+              <linearGradient
+                id="paint0_linear_970_280"
+                x1="4.54647"
+                y1="-5.28981e-05"
+                x2="616.249"
+                y2="213.967"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#27272A" />
+                <stop offset="1" stop-color="#0E0E0F" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <div tw="flex flex-col p-10 w-full">
+            <div tw="flex absolute top-10 right-10">
+              <Logo3d />
+            </div>
+
+            <p tw="flex items-center -mt-3">
+              <p tw="text-2xl font-bold bg-zinc-600 rounded-full px-3 py-1.5">
+                <span>@</span>
+                <span>{props.username}</span>
+              </p>
+              <p tw="text-2xl ml-4">{props.date}</p>
             </p>
-            {/* TODO: replace with date posted */}
-            <p tw="text-3xl ml-4">x days ago</p>
-          </p>
-          <p tw="flex items-center">
-            {/* TODO: replace with props.difficulty */}
-            <p tw="bg-blue-400 text-black font-bold text-2xl px-3 py-1.5 rounded-full">BEGINNER</p>
-          </p>
-          <p tw="text-center text-3xl text-zinc-300">{props.description}</p>
+            <h1
+              tw="text-7xl font-bold truncate pr-48 -mt-2 overflow-ellipsis"
+              style={{ fontWeight: '800 !important' }}
+            >
+              {props.title}
+            </h1>
+            <p tw="flex items-center -mt-3">
+              <p
+                tw={`${
+                  COLORS_BY_DIFFICULTY[props.difficulty]
+                } text-black font-bold text-2xl px-6 py-1.5 rounded-full`}
+              >
+                {props.difficulty}
+              </p>
+            </p>
+            <p tw="text-3xl text-zinc-300">{props.description}</p>
+          </div>
         </div>
       </div>
     ),
