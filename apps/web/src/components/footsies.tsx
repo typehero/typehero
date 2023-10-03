1b8cb797220a2ad0a8f76fd19830cc987b654be3
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-10-03 18:37:48 -0400

    Footer adjustments (#765)
    * copyleft
    
    * copyleft
    
    * better footer layout
    
    * fix mobile footer
    
    ---------
    
    Co-authored-by: bautistaaa <chrisbautistaaa@gmail.com>

--- apps/web/src/components/footsies.tsx
+++ apps/web/src/components/footsies.tsx
@@ -4,28 +4,29 @@ import Link from 'next/link';
 export function Footsies() {
   return (
     <footer className="flex flex-col items-center text-sm font-light sm:px-16 md:px-0">
-      <div className="container my-4 flex flex-col justify-between gap-6 px-10 pb-2 md:my-12 lg:flex-row">
-        <div className="space-x-2">
+      <div className="container my-4 flex flex-col justify-between gap-2 px-10 pb-2 md:my-12 lg:flex-row lg:items-end">
+        <div>
           Built with <Binary className="inline-block h-5 w-5 text-[#31bdc6]" /> by the Trash Devs
           community.
+          <div className="mt-2 text-neutral-500 dark:text-neutral-400">
+            <Link
+              href="/privacy"
+              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+            >
+              Privacy Policy
+            </Link>{' '}
+            |{' '}
+            <Link
+              href="/tos"
+              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+            >
+              Terms of Service
+            </Link>
+          </div>
         </div>
-        <div className="text-neutral-500 dark:text-neutral-400">
-          <Link
-            href="/privacy"
-            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-          >
-            Privacy Policy
-          </Link>{' '}
-          |{' '}
-          <Link
-            href="/tos"
-            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-          >
-            Terms of Service
-          </Link>
-        </div>
-        <div className="text-neutral-500 dark:text-neutral-400">
-          © {new Date().getFullYear()} TypeHero
+        <div className="text-neutral-500 dark:text-neutral-400 lg:text-right">
+          <div className="inline-block rotate-180">©</div>
+          {new Date().getFullYear()} TypeHero
         </div>
       </div>
     </footer>
