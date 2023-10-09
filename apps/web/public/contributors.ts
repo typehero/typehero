39466e212dc1c79c0bb164e6790afe993ac88827
QuiiBz <QuiiBz@users.noreply.github.com>
2023-10-09 01:15:00 +0000

    chore: update contributors

--- apps/web/public/contributors.ts
+++ apps/web/public/contributors.ts
@@ -20,7 +20,7 @@ export const contributors = [
         "received_events_url": "https://api.github.com/users/bautistaaa/received_events",
         "type": "User",
         "site_admin": false,
-        "contributions": 178
+        "contributions": 180
     },
     {
         "login": "Hacksore",
@@ -41,7 +41,7 @@ export const contributors = [
         "received_events_url": "https://api.github.com/users/Hacksore/received_events",
         "type": "User",
         "site_admin": false,
-        "contributions": 85
+        "contributions": 86
     },
     {
         "login": "PickleNik",
