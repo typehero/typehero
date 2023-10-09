526b43c51c9821561099d427d27f2bb850ade7fe
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-10-09 11:33:12 +0530

    chore: bye bye `/<user>/settings`

--- apps/web/src/app/[locale]/settings/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function ErrorBounds() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p className="text-2xl">The page you are looking for is in another castle.</p>
-    </div>
-  );
-}
