4502f07b4a7bb25f7de9f14823a4e7b798c80dd8
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-10-11 11:18:25 -0400

    update user og with grid & fonts

--- apps/og-image/app/api/user/route.tsx
+++ apps/og-image/app/api/user/route.tsx
@@ -1,10 +1,18 @@
 import { ImageResponse } from '@vercel/og';
-import { Logo } from 'app/components/logo';
 import { userParam } from 'utils/zodParams';
+import { fetchFont } from 'utils/fetchFont';
+import { Grid } from 'app/components/grid';
+import { Logo } from 'app/components/logo';
 
 export const runtime = 'edge';
 
 export async function GET(req: Request) {
+  const [inter900, inter700, inter400] = await Promise.all([
+    fetchFont('Inter', 900),
+    fetchFont('Inter', 700),
+    fetchFont('Inter', 400),
+  ]);
+
   const parsed = userParam.decodeRequest(req);
 
   if (!parsed.success) {
@@ -15,11 +23,12 @@ export async function GET(req: Request) {
 
   return new ImageResponse(
     (
-      <div tw="bg-zinc-900 h-full w-full text-white flex flex-col p-14">
-        <div tw="flex flex-col justify-center items-center w-full h-full">
+      <div tw="bg-black h-full w-full text-white bg-cover flex flex-col px-32 py-14">
+        <Grid />
+        <div tw="flex flex-col justify-center items-center text-center h-full w-full">
           <Logo />
-          <h1 tw="text-6xl pt-3">{props.username}</h1>
-          <p tw="text-center text-3xl text-zinc-300">
+          <h1 tw="text-6xl pt-3 font-bold">{props.username}</h1>
+          <p tw="text-3xl text-zinc-300">
             A user that we can prolly pull data from the db or at least their avatar maybe?
           </p>
         </div>
@@ -28,6 +37,11 @@ export async function GET(req: Request) {
     {
       width: 1200,
       height: 600,
+      fonts: [
+        { name: 'Inter', data: inter900, weight: 900 },
+        { name: 'Inter', data: inter700, weight: 700 },
+        { name: 'Inter', data: inter400, weight: 400 },
+      ],
     },
   );
 }
