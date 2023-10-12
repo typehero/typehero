5dbcafcccdaaf96d28e4a54985469052dd36e942
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-10-11 21:08:02 -0400

    Footer socials (#843)
    * add social links to footer and do abunch of other stuff for some reason
    
    * cleanup footer styles

--- apps/web/src/app/[locale]/_components/hero-illustration.tsx
+++ apps/web/src/app/[locale]/_components/hero-illustration.tsx
@@ -4,6 +4,20 @@ import { motion } from 'framer-motion';
 import { useIsMobile } from '~/utils/useIsMobile';
 import { HeroChallengeCard } from './hero-challenge-card';
 
+export function BackgroundGrid() {
+  return (
+    <div className="relative h-full w-full">
+      <motion.div
+        animate={{ opacity: 1 }}
+        className="moving-grid-background absolute h-[200%] w-full"
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.6, delay: 0.6 }}
+      />
+      <div className="shadow-background absolute h-full w-full rounded-full shadow-[inset_0_0_5rem_3rem]" />
+    </div>
+  );
+}
+
 export function HeroIllustration() {
   const isMobile = useIsMobile();
 
@@ -14,16 +28,9 @@ export function HeroIllustration() {
   return (
     <div className="relative hidden h-[800px] overflow-visible rounded-full lg:block">
       <div className="absolute -inset-40 top-1/2 -z-30 -translate-y-1/2 translate-x-[-30px] overflow-hidden rounded-full">
-        <div className="relative h-full w-full">
-          <motion.div
-            animate={{ opacity: 1 }}
-            className="moving-grid-background absolute h-[200%] w-full"
-            initial={{ opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.6 }}
-          />
-          <div className="shadow-background absolute h-full w-full rounded-full shadow-[inset_0_0_5rem_3rem]" />
-        </div>
+        <BackgroundGrid />
       </div>
+
       <motion.div
         animate={{
           y: 140,
--- apps/web/src/components/footsies.tsx
+++ apps/web/src/components/footsies.tsx
@@ -1,33 +1,53 @@
-import { Binary } from '@repo/ui/icons';
+import { Binary, Github, Twitter } from '@repo/ui/icons';
 import Link from 'next/link';
 
 export function Footsies() {
   return (
-    <footer className="flex flex-col items-center text-sm font-light sm:px-16 md:px-0">
-      <div className="container my-4 flex flex-col justify-between gap-2 px-10 pb-2 md:my-12 lg:flex-row lg:items-end">
-        <div>
+    <footer className="flex flex-col items-center gap-2 px-8 pb-12 text-sm font-light sm:px-16 sm:pb-20 sm:pt-6 md:px-0 md:py-12">
+      <div className="container flex flex-col-reverse justify-between gap-2 md:flex-row md:items-end">
+        <span>
           Built with <Binary className="inline-block h-5 w-5 text-[#31bdc6]" /> by the Trash Devs
           community.
-          <div className="mt-2 text-neutral-500 dark:text-neutral-400">
-            <Link
-              href="/privacy"
-              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-            >
-              Privacy Policy
-            </Link>{' '}
-            |{' '}
-            <Link
-              href="/tos"
-              className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
-            >
-              Terms of Service
-            </Link>
-          </div>
+        </span>
+        <div className="flex gap-2">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="group gap-1 md:inline-flex"
+            href="https://github.com/typehero/typehero"
+          >
+            <Github className="h-4 w-4 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="group gap-1 md:inline-flex"
+            href="https://twitter.com/typeheroapp"
+          >
+            <Twitter className="h-4 w-4 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white" />
+          </a>
         </div>
-        <div className="text-neutral-500 dark:text-neutral-400 lg:text-right">
+      </div>
+      <div className="container flex flex-col justify-between gap-2 text-neutral-500 dark:text-neutral-400  md:flex-row md:items-end">
+        <span>
+          <Link
+            href="/privacy"
+            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+          >
+            Privacy Policy
+          </Link>{' '}
+          |{' '}
+          <Link
+            href="/tos"
+            className="dark:hover:text-primary-foreground transition-colors duration-300 hover:text-neutral-900 hover:underline"
+          >
+            Terms of Service
+          </Link>
+        </span>
+        <span>
           <div className="inline-block rotate-180">©</div>
           {new Date().getFullYear()} TypeHero
-        </div>
+        </span>
       </div>
     </footer>
   );
