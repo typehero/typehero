2a12392d3948f87396a5728d4c0728e12e76ac7d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-14 19:52:33 -0500

    Fix meta with this template thing 💀 (#867)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: 'TypeHero',
+    default: 'Home',
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
