6ccefb6f17573d6f4688f2cb3858e18a77016d17
Malki Abdurrahman <malki.abdurrahmane@gmail.com>
2023-10-16 17:47:18 +0100

    Update the grid size and opacity

--- apps/og-image/app/components/grid.tsx
+++ apps/og-image/app/components/grid.tsx
@@ -4,10 +4,10 @@ export function Grid() {
       // @ts-ignore
       tw="absolute h-[150%] w-[150%]"
       style={{
-        backgroundSize: '81px 52px',
-        backgroundPosition: '43% 48%',
+        backgroundSize: '100px 60px',
+        backgroundPosition: '55% 45%',
         backgroundImage:
-          'linear-gradient(#fff5 .1px, transparent 1px),linear-gradient(to right, #fff5 .1px, transparent 1px)',
+          'linear-gradient(#fffe .1px, transparent 1px),linear-gradient(to right, #fffe .1px, transparent 1px)',
       }}
     ></div>
   );
