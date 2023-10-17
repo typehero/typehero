1704c6e15c8d739742ee5f70971ce6420844ee12
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-10-17 00:11:16 -0400

    undo

--- apps/web/src/app/[locale]/_components/hero-illustration.tsx
+++ apps/web/src/app/[locale]/_components/hero-illustration.tsx
@@ -6,13 +6,14 @@ import { HeroChallengeCard } from './hero-challenge-card';
 
 export function BackgroundGrid() {
   return (
-    <div className="gridmask relative h-full w-full">
+    <div className="relative h-full w-full">
       <motion.div
         animate={{ opacity: 1 }}
         className="moving-grid-background absolute h-[200%] w-full"
         initial={{ opacity: 0 }}
         transition={{ duration: 0.6, delay: 0.6 }}
       />
+      <div className="shadow-background absolute h-full w-full rounded-full shadow-[inset_0_0_5rem_3rem]" />
     </div>
   );
 }
