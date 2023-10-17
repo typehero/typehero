1ba3fa1fe683cf277c40c3edf818a43fa9407cd2
bautistaaa <chrisbautistaaa@gmail.com>
2023-10-17 10:28:27 -0700

    chore: ignore challenges

---
+++ packages/db/seed/data/tracks.ts
@@ -0,0 +1,27 @@
+export const tracks = [
+  {
+    name: 'Crafting TypeScript Utility Types',
+    description:
+      "This collection guides you through hands-on exercises to recreate Typescript's built-in utility types.",
+  },
+  {
+    name: 'Typescript Wizardry',
+    description:
+      "Dive into the elite realm of TypeScript mastery with our 'Pro-Level TypeScript Challenges.' Designed for seasoned professionals, this collection offers a formidable gauntlet of intricate exercises, pushing the boundaries of your TypeScript expertise to the limit.",
+  },
+  {
+    name: 'Javascript Built in Methods',
+    description:
+      "This collection of challenges equips you to develop and enhance standard JavaScript methods, all while harnessing TypeScript's advanced type system to achieve a fully typesafe developer experience.",
+  },
+  {
+    name: 'Understanding Typescript Syntax',
+    description:
+      'A collection of challenging exercises that dive into type annotations, generics, and more to level up your typescript abilities.',
+  },
+  {
+    name: 'Typescript Foundations',
+    description:
+      "'Typescript Foundations' is a curated set of challenges designed to build a strong foundation. From basic syntax to advanced concepts, this collection offers hands-on exercises to help you become a TypeScript Hero.",
+  },
+];
