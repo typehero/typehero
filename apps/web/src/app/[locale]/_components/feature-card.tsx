aa0ea4ef8069d406699758a0f4de90ea9a0f7556
Boris Nezlobin <146669165+borisnezlobin@users.noreply.github.com>
2023-10-18 11:23:31 -0700

    Fix landing page on small screens. (#758)
    * fix: landing page on small screens
    
    * make cards smaller on smaller screense, make card content more responsive
    
    * rebase
    
    * fix card #3 (which may be removed anyways)
    
    * Footer socials (#843)
    
    * add social links to footer and do abunch of other stuff for some reason
    
    * cleanup footer styles
    
    * resolve conflicts
    
    * rebase + resolve conflicts
    
    * idfk atp
    
    * fix: typo in tailwind rule
    
    * fix: logo?
    
    * minor fixes
    
    * Update navigation.tsx with nikita's suggested changes
    
    Co-authored-by: PickleNik <31113245+PickleNik@users.noreply.github.com>
    
    * Update navigation.tsx again
    
    Co-authored-by: PickleNik <31113245+PickleNik@users.noreply.github.com>
    
    * Update apps/web/src/app/[locale]/_components/feature-card.tsx
    
    ---------
    
    Co-authored-by: PickleNik <31113245+PickleNik@users.noreply.github.com>

--- apps/web/src/app/[locale]/_components/feature-card.tsx
+++ apps/web/src/app/[locale]/_components/feature-card.tsx
@@ -73,7 +73,7 @@ function FeatureCard({
           bgClass,
         )}
       >
-        <div className="mx-10 my-10 min-h-[450px] w-full">
+        <div className="mx-10 my-10 min-h-[250px] w-full md:min-h-[450px]">
           <div className="flex w-4/6 flex-col gap-3 md:w-[80%] xl:w-4/6">
             <h2 className="text-xl font-bold tracking-tight md:text-xl">{title}</h2>
             <p className="text-sm leading-5 text-zinc-600 dark:text-zinc-400 sm:text-base sm:leading-7">
@@ -204,7 +204,7 @@ export function ChallengeCreationCard({
       <div
         className={clsx(
           { 'translate-x-0 opacity-0': step < 3 },
-          'absolute left-1/2 top-1/2 flex w-[80%] -translate-x-1/2 -translate-y-[33%] flex-col gap-12 text-center text-2xl font-bold transition-all duration-500 md:w-[50%]',
+          'absolute left-1/2 top-[55%] flex w-[80%] -translate-x-1/2 -translate-y-[33%] flex-col gap-12 text-center text-2xl font-bold transition-all duration-500 md:w-[50%]',
         )}
       >
         <div>🎉</div>
@@ -391,7 +391,7 @@ export function CollaborativeEnvironmentCard(props: CardProps) {
     <FeatureCard {...props}>
       <div
         ref={ref}
-        className="absolute inset-0 left-[23px] top-[30%] flex w-[100%] flex-col gap-3 pt-4 max-md:scale-90 sm:top-[35%] md:left-[37px] md:top-[30%]"
+        className="absolute inset-0 left-[23px] top-[50%] flex w-[100%] flex-col gap-3 pt-4 max-md:scale-90 sm:top-[35%] md:left-[37px] md:top-[30%]"
       >
         <div
           className={clsx(
@@ -402,7 +402,7 @@ export function CollaborativeEnvironmentCard(props: CardProps) {
         >
           <div className="flex items-center gap-2">
             <Badge name="dax" />
-            <div className="text-xs text-neutral-500">5 years ago</div>
+            <div className="text-xs text-neutral-500">48 minutes ago</div>
           </div>
           Implementing Pick in TypeScript is hard, can anyone help?
         </div>
@@ -416,7 +416,7 @@ export function CollaborativeEnvironmentCard(props: CardProps) {
           <Reply className="absolute -left-8 h-4 w-4 opacity-50" />
           <div className="flex items-center gap-2">
             <Badge name="trash" />
-            <div className="text-xs text-neutral-500">just now</div>
+            <div className="text-xs text-neutral-500">12 minutes ago</div>
           </div>
           ez, the answer is
           <Markdown>{solutionComment}</Markdown>
@@ -451,7 +451,7 @@ export function CuratedTracksCard(props: CardProps) {
     <FeatureCard {...props}>
       <div
         className={clsx(
-          'absolute inset-0 top-[50%] flex flex-col items-center justify-center',
+          'absolute inset-0 top-[90%] flex flex-col items-center justify-center',
           'max-md:scale-110 md:top-[38%]',
         )}
       >
@@ -460,7 +460,7 @@ export function CuratedTracksCard(props: CardProps) {
             TypeScript Foundations
           </span>
         </div>
-        <div className="flex w-[69%] flex-col md:w-[82%] xl:w-[69%]">
+        <div className="flex w-[69%] flex-col sm:top-auto md:w-[82%] xl:w-[69%]">
           {mockChallenges.map((mockChallenge) => (
             <MockTrackChallenge
               key={`mock-${mockChallenge.id}`}
