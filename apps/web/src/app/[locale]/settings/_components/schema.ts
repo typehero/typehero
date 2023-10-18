9ee244749a731af0eace7d5b38127284bc9abf45
Eric Lee <ayrocklee@gmail.com>
2023-10-17 19:28:29 -0700

    feat: update `/settings` for early access (#892)
    * feat: update `/settings` for early access
    
    * chore: remove appearance tab
    
    * feat: update titles and icons

--- apps/web/src/app/[locale]/settings/_components/schema.ts
+++ apps/web/src/app/[locale]/settings/_components/schema.ts
@@ -14,19 +14,6 @@ export const appearanceFormSchema = z.object({
 export type ApperenceFormSchema = z.infer<typeof appearanceFormSchema>;
 
 export const profileSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: 'Username must be at least 2 characters.',
-    })
-    .max(30, {
-      message: 'Username must not be longer than 30 characters.',
-    }),
-  email: z
-    .string({
-      required_error: 'Please select an email to display.',
-    })
-    .email(),
   bio: createNoProfanitySchemaWithValidate((str) => str.max(256)),
   userLinks: z.array(
     z.object({
