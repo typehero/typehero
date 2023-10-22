011ac184639b0ca9531f5127d3bea524f611a47e
Shrikrishna Joisa <shrikrishnajois@gmail.com>
2023-10-22 18:35:32 -0400

    Handle boomer www and subdomains

--- packages/ui/src/components/magic-icon.tsx
+++ packages/ui/src/components/magic-icon.tsx
@@ -1,7 +1,5 @@
 import { Github, Link as LinkIcon, Linkedin, Twitter, Youtube } from 'lucide-react';
 
-// TODO: this could be more robust
-// it's not accounting for subdomains like the boomer www and stuff
 export function MagicIcon({ url }: { url: string }) {
   const githubRegex = /^https?:\/\/(?:www\.)?github\.com\//;
   const twitterRegex = /^https?:\/\/(?:www\.)?twitter\.com\//;
