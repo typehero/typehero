700b294ac115799a46069626affc2f62a3e4936f
bautistaaa <chrisbautistaaa@gmail.com>
2023-10-24 12:21:43 -0700

    looks like we cant even borrow the values

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,41 @@
-import { baseMetadata } from './metadata';
+import { OG_URL, tagline } from './metadata';
 
 export const metadata = {
-  ...baseMetadata,
+  metadataBase: new URL(OG_URL),
   title: 'TypeHero',
+  robots: {
+    index: true,
+    follow: true,
+  },
+  description: tagline,
+  openGraph: {
+    title: 'TypeHero',
+    description: tagline,
+    siteName: 'TypeHero',
+    images: [
+      {
+        url: `${OG_URL}/api/default`,
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  twitter: {
+    title: 'TypeHero',
+    card: 'summary_large_image',
+    images: [
+      {
+        url: `${OG_URL}/api/default`,
+        width: 1920,
+        height: 1080,
+      },
+    ],
+  },
+  icons: {
+    shortcut: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
