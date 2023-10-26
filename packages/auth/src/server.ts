9bc7ed5a5df143fb9f590d2c5d79317220977528
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-25 19:44:04 -0500

    Remove cb url

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -51,16 +51,6 @@ export const authOptions: NextAuthOptions = {
         secure: useSecureCookies,
       },
     },
-    callbackUrl: {
-      name: `${useSecureCookies ? '__Secure-' : ''}next-auth.callback-url`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        domain: useSecureCookies ? 'typehero.dev' : process.env.VERCEL_URL,
-        secure: useSecureCookies,
-      },
-    },
     csrfToken: {
       name: '__Host-next-auth.csrf-token',
       options: {
