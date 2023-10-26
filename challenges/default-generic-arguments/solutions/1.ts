040a750d230ab0c436015402b0d43097ad00342e
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-10-25 21:05:03 -0400

    new(challenge): default-generic-arguments

---
+++ challenges/default-generic-arguments/solutions/1.ts
@@ -0,0 +1,8 @@
+type ApiRequest<Data, Method = 'GET'> = {
+  data: Data;
+  method: Method;
+};
+
+type TSConfig<Config extends { strict: boolean } = { strict: true }> = {
+  strict: Config['strict'];
+}
