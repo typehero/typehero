0df479c47b884ceef9eeb188ffa80cc17bae4e50
PickleNik <31113245+PickleNik@users.noreply.github.com>
2023-10-27 19:03:49 -0400

    Update enroll-button.tsx

--- apps/web/src/app/[locale]/tracks/_components/enroll-button.tsx
+++ apps/web/src/app/[locale]/tracks/_components/enroll-button.tsx
@@ -17,7 +17,7 @@ export function ActionButton({ action, text, trackId }: EnrollButtonProps) {
   const [isLoading, setIsLoading] = useState(false);
   return (
     <Button
-      className="shadcnoverridetransition select-none rounded-3xl bg-black text-white shadow-[0px_16px_6px_-16px_#ff4,4px_2px_4px_-2px_#f4f,-4px_2px_4px_-2px_#4f4] hover:-translate-y-1 hover:bg-neutral-800 hover:shadow-[0px_16px_6px_-12px_#ff4,4px_6px_6px_-2px_#f4f,-4px_6px_6px_-2px_#4f4] active:translate-y-1 active:scale-95 active:shadow-[0px_10px_3px_-16px_#ff4,8px_0px_2px_-2px_#f4f,-8px_0px_2px_-2px_#4f4] dark:bg-white dark:text-black dark:hover:bg-white dark:hover:brightness-125 dark:hover:saturate-150"
+      className="shadcnoverridetransition select-none rounded-3xl bg-black text-white shadow-[0px_16px_6px_-16px_#ff4,4px_2px_4px_-2px_#f4f,-4px_2px_4px_-2px_#4f4] hover:-translate-y-1 hover:bg-neutral-800 hover:shadow-[0px_16px_6px_-12px_#ff4,4px_6px_6px_-2px_#f4f,-4px_6px_6px_-2px_#4f4] active:translate-y-0 active:scale-95 active:shadow-[0px_10px_3px_-16px_#ff4,8px_0px_2px_-2px_#f4f,-8px_0px_2px_-2px_#4f4] dark:bg-white dark:text-black dark:hover:bg-white dark:hover:brightness-125 dark:hover:saturate-150"
       disabled={isLoading}
       onClick={async () => {
         setIsLoading(true);
