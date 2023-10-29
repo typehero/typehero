ba0872a9a98aa95d52f3e2e5323a580e5c27ab64
niemal <niemal@gmx.com>
2023-10-29 20:04:43 +0200

    fix: index-signatures challenge proper test cases

--- challenges/index-signatures/tests.ts
+++ challenges/index-signatures/tests.ts
@@ -1,3 +1,11 @@
+import { NotEqual, Expect } from 'type-testing';
+
+type testCaseGroceryList = Expect<NotEqual<GroceryList, unknown>>;
+type testCaseInappropriateActionBySituation = Expect<
+  NotEqual<InappropriateActionBySituation, unknown>
+>;
+type testCaseCharactersById = Expect<NotEqual<CharactersById, unknown>>;
+
 const groceryList: GroceryList = {
   carrots: 5,
   potatoes: 12,
@@ -14,45 +22,56 @@ const groceryList: GroceryList = {
   shouldError: "because it's a string",
 
   // @ts-expect-error intentionally invalid
-  shouldAlsoError: true
+  shouldAlsoError: true,
 };
 
 const inappropriateActionBySituation: InappropriateActionBySituation = {
   funeral: ['excessive laughter', 'bringing up personal achievements'],
-  medicalDiagnosis: ['jokes about American healthcare', 'WebMD', 'doomscrolling twitter instead of listening'],
-  leetcodeInterview: ['praise of CSS', 'citing XKCD comics by number from memory', 'use of emojis in whiteboard exercises followed by pontificating about your deep knowledge of UTF-16'],
-  friendExperiencingHeartbreak: ['victory dance because you hated their S.O.', 'offers to turn on the 1999 cinematic masterpiece, The Mummy, with Brendan Fraser and Rachel Weisz'],
+  medicalDiagnosis: [
+    'jokes about American healthcare',
+    'WebMD',
+    'doomscrolling twitter instead of listening',
+  ],
+  leetcodeInterview: [
+    'praise of CSS',
+    'citing XKCD comics by number from memory',
+    'use of emojis in whiteboard exercises followed by pontificating about your deep knowledge of UTF-16',
+  ],
+  friendExperiencingHeartbreak: [
+    'victory dance because you hated their S.O.',
+    'offers to turn on the 1999 cinematic masterpiece, The Mummy, with Brendan Fraser and Rachel Weisz',
+  ],
 };
 
 const charactersById: CharactersById = {
   1: {
     id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
+    name: 'Rick Sanchez',
+    status: 'Alive',
+    species: 'Human',
   },
   2: {
     id: 2,
-    name: "Morty Smith",
-    status: "Alive",
-    species: "Human",
+    name: 'Morty Smith',
+    status: 'Alive',
+    species: 'Human',
   },
   3: {
     id: 3,
-    name: "Summer Smith",
-    status: "Alive",
-    species: "Human",
+    name: 'Summer Smith',
+    status: 'Alive',
+    species: 'Human',
   },
   4: {
     id: 4,
-    name: "Beth Smith",
-    status: "Alive",
-    species: "Human",
+    name: 'Beth Smith',
+    status: 'Alive',
+    species: 'Human',
   },
   5: {
     id: 5,
-    name: "Jerry Smith",
-    status: "Alive",
-    species: "Human",
+    name: 'Jerry Smith',
+    status: 'Alive',
+    species: 'Human',
   },
 };
