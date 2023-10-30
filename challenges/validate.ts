70221f05229d7d598fe53ff50aa5ba28fb8304d3
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-10-30 18:11:38 -0400

    adds solution runner

--- challenges/validate.ts
+++ challenges/validate.ts
@@ -1,11 +1,23 @@
 import { readdirSync, readFileSync, statSync } from 'fs';
 import { join } from 'path';
 import Ajv from 'ajv';
+import {
+  createCompilerHost,
+  createProgram,
+  createSourceFile,
+  flattenDiagnosticMessageText,
+  getPreEmitDiagnostics,
+  readConfigFile,
+  ScriptTarget,
+  sys,
+} from 'typescript';
 
 /*
 this script does a few checks:
 
 1. makes sure each directory has a metadata.json file
+1. makes sure each directory has a tsconfig.json file
+1. makes sure each directory has a solutions directory with at least one solution file
 1. runs JSONSchema validation on all metadata.json files
 1. make sure the directory name matches the id contained within it's metadata.json file
   - (bonus: this ensures that the id is unique since you can't have two sibling directories with the same name)
@@ -15,11 +27,12 @@ this script does a few checks:
 
 const dir = __dirname;
 
-const getChallengeIds = () => {
+export const getChallengeIds = () => {
   return readdirSync(dir)
-    .filter((entry) => entry !== 'blank')
-    .filter((entry) => statSync(join(dir, entry)).isDirectory())
-    .filter((entry) => statSync(join(dir, entry, 'metadata.json')).isFile());
+    .filter((id) => id !== 'blank')
+    .filter((id) => statSync(join(dir, id)).isDirectory())
+    .filter((id) => statSync(join(dir, id, 'metadata.json')).isFile())
+    .filter((id) => statSync(join(dir, id, 'tsconfig.json')).isFile());
 };
 
 const getMetadata = (id: string) => {
@@ -91,4 +104,56 @@ const validateMetadataFiles = () => {
   challengeIds.forEach(validatePrerequisiteIds);
 };
 
-validateMetadataFiles();
+const validateTests = () => {
+  getChallengeIds()
+    .filter((id) => {
+      const path = join(dir, id, 'solutions');
+      statSync(path).isDirectory();
+      readdirSync(path).forEach((file) => {
+        if (!/^\d+\.ts$/.test(file)) {
+          throw new Error(
+            `the solution file name: ${join(path, file)} is invalid: it must only be numeric.`,
+          );
+        }
+      });
+      return true;
+    })
+    .forEach((id) => {
+      const tsconfig = readConfigFile(join(dir, id, 'tsconfig.json'), sys.readFile).config
+        .compilerOptions;
+      const testsSource = readFileSync(join(dir, id, 'tests.ts')).toString();
+
+      readdirSync(join(dir, id, 'solutions')).forEach((file) => {
+        const solutionPath = join(dir, id, 'solutions', file);
+        const solutionSource = readFileSync(solutionPath).toString();
+
+        const sourceFileName = `in-memory/${id}/${file}`;
+
+        const compilerHost = createCompilerHost(tsconfig, true);
+        const program = createProgram([sourceFileName], tsconfig, compilerHost);
+
+        console.log(`checking ${solutionPath}`);
+        const sourceFile = createSourceFile(
+          sourceFileName,
+          `${testsSource}\n${solutionSource}`,
+          ScriptTarget.Latest,
+        );
+        console.log('sourceFile', sourceFile);
+
+        getPreEmitDiagnostics(program, sourceFile).forEach((diagnostic) => {
+          const message = flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+
+          console.error(
+            `[ERROR] challenge solution \`${id}/solutions/${file}\` contains the following error when run against the tests: ${message}`,
+          );
+        });
+      });
+    });
+};
+
+const validate = () => {
+  validateMetadataFiles();
+  validateTests();
+};
+
+validate();
