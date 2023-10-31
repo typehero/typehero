024b8545fdf8e2e431d9f835b2354e2d860150ed
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-10-31 13:54:40 -0400

    fix all test errors except use-before-declare

--- challenges/primitive-data-types/solutions/1.ts
+++ challenges/primitive-data-types/solutions/1.ts
@@ -1,23 +1,23 @@
-const playSong = (name: string, year: number) => {
-  return `${name} was released in the year ${year}`;
+const playSong = (artistName: string, year: number) => {
+  return `${artistName} was released in the year ${year}`;
 };
 
-const name: string = 'Frank Zappa';
+const artistName: string = 'Frank Zappa';
 
 const age: number = 52;
 
 interface Musician {
-  name: string;
+  artistName: string;
   age: number;
   deceased: boolean;
 }
 
-const musicianInfo = ({ name, age, deceased }: Musician) => {
-  return `${name}, age ${age}${deceased ? ' (deceased)' : ''}`;
+const musicianInfo = ({ artistName, age, deceased }: Musician) => {
+  return `${artistName}, age ${age}${deceased ? ' (deceased)' : ''}`;
 };
 
 musicianInfo({
-  name,
+  artistName,
   age,
   deceased: true,
 });
