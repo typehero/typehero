901c7ea64ea4bbd6f979dcc737c5c76c66771b7a
Zach Lankton <zachlankton@gmail.com>
2023-11-09 12:39:55 -0500

    remove test zz locale

--- apps/web/src/locales/server.ts
+++ apps/web/src/locales/server.ts
@@ -2,5 +2,4 @@ import { createI18nServer } from 'next-international/server';
 
 export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
   en: () => import('./en'),
-  zz: () => import('./test'),
 });
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,7 +2,7 @@ import { createI18nMiddleware } from 'next-international/middleware';
 import { type NextRequest } from 'next/server';
 
 const I18nMiddleware = createI18nMiddleware({
-  locales: ['en', 'zz'],
+  locales: ['en'],
   defaultLocale: 'en',
   urlMappingStrategy: 'rewrite',
 });
