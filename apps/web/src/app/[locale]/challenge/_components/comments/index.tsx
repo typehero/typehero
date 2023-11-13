e1f005a4c9c44ba28fc25e225b2339217164f5c5
__aadamw <131662061+aadamw@users.noreply.github.com>
2023-11-13 18:13:58 +0100

    [Feat] Refactor comments CRUD (#1103)
    * Refactor comments logic
    
    * Remove deadcode
    
    * fix
    
    ---------
    
    Co-authored-by: Ayush Kumar Yadav <69889382+zaCKoZAck0@users.noreply.github.com>
    Co-authored-by: bautistaaa <chrisbautistaaa@gmail.com>

--- apps/web/src/app/[locale]/challenge/_components/comments/index.tsx
+++ apps/web/src/app/[locale]/challenge/_components/comments/index.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import type { CommentRoot } from '@repo/db/types';
-import { toast } from '@repo/ui/components/use-toast';
 import { ChevronDown, MessageCircle } from '@repo/ui/icons';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { useRef, useState } from 'react';
 import { Comment } from './comment';
 import { CommentInput } from './comment-input';
 import { CommentSkeleton } from './comment-skeleton';
-import { addComment } from './comment.action';
 import { getPaginatedComments, type PreselectedCommentMetadata } from './getCommentRouteData';
 import NoComments from './nocomments';
 import { Pagination } from '../pagination';
 import { SortSelect } from '../sort-select';
+import { useComments } from './comments.hooks';
+import { sortKeys } from './comments.constants';
 
 interface Props {
   preselectedCommentMetadata?: PreselectedCommentMetadata;
@@ -22,96 +21,30 @@ interface Props {
   type: CommentRoot;
 }
 
-const sortKeys = [
-  {
-    label: 'Newest Comments',
-    value: 'newest',
-    key: 'createdAt',
-    order: 'desc',
-  },
-  {
-    label: 'Oldest Comments',
-    value: 'oldest',
-    key: 'createdAt',
-    order: 'asc',
-  },
-  {
-    label: 'Most Votes',
-    value: 'votes',
-    key: 'vote',
-    order: 'desc',
-  },
-  {
-    label: 'Most Replies',
-    value: 'replies',
-    key: 'replies',
-    order: 'desc',
-  },
-] as const;
-
 // million-ignore
 export function Comments({ preselectedCommentMetadata, rootId, type, expanded = false }: Props) {
   const [showComments, setShowComments] = useState(expanded);
-  const [text, setText] = useState('');
   const commentContainerRef = useRef<HTMLDivElement>(null);
-  const queryClient = useQueryClient();
-  const [page, setPage] = useState(preselectedCommentMetadata?.page ?? 1);
-  const [sortKey, setSortKey] = useState<(typeof sortKeys)[number]>(sortKeys[0]);
-  const queryKey = [`${type.toLowerCase()}-${rootId}-comments`, sortKey.value, page];
-  const { status, data } = useQuery({
-    queryKey,
-    queryFn: () =>
-      getPaginatedComments({
-        rootId,
-        page,
-        rootType: type,
-        sortKey: sortKey.key,
-        sortOrder: sortKey.order,
-      }),
-    staleTime: 60000, // one minute
-    refetchOnWindowFocus: false,
+  const {
+    data,
+    addComment,
+    commentsMeta,
+    status,
+    changePage,
+    changeSorting,
+    deleteComment,
+    updateComment,
+  } = useComments({
+    type,
+    rootId,
   });
 
-  async function createChallengeComment() {
-    try {
-      const res = await addComment({
-        text,
-        rootId,
-        rootType: type,
-      });
-      if (res === 'text_is_empty') {
-        toast({
-          title: 'Empty Comment',
-          description: <p>You cannot post an empty comment.</p>,
-        });
-      } else if (res === 'unauthorized') {
-        toast({
-          title: 'Unauthorized',
-          description: <p>You need to be signed in to post a comment.</p>,
-        });
-      }
-      setText('');
-      queryClient.invalidateQueries({ queryKey: [`${type.toLowerCase()}-${rootId}-comments`] });
-    } catch (e) {
-      toast({
-        title: 'Unauthorized',
-        variant: 'destructive',
-        description: <p>You need to be signed in to post a comment.</p>,
-      });
-    }
-  }
-
   const handleChangePage = (page: number) => {
-    setPage(page);
+    changePage(page);
     commentContainerRef.current?.scroll({
       top: 128,
       behavior: 'smooth',
     });
-  };
-
-  const handleValueChange = (value: string) => {
-    setSortKey(sortKeys.find((sk) => sk.value === value) ?? sortKeys[0]);
-    setPage(1);
   };
 
   return (
@@ -133,7 +66,7 @@ export function Comments({ preselectedCommentMetadata, rootId, type, expanded = 
           <div className="flex items-center gap-2">
             <MessageCircle className="h-5 w-5" />
             Comments
-            {data?.totalComments != null && <span>({data.totalComments})</span>}
+            {data?.totalComments != null ? <span>({data.totalComments})</span> : null}
           </div>
           <ChevronDown
             className={clsx('h-4 w-4 duration-300', {
@@ -153,34 +86,30 @@ export function Comments({ preselectedCommentMetadata, rootId, type, expanded = 
           ref={commentContainerRef}
         >
           <div className="m-2 mb-4 mt-0">
-            <CommentInput
-              mode="create"
-              onChange={setText}
-              onSubmit={createChallengeComment}
-              value={text}
-            />
+            <CommentInput mode="create" onSubmit={addComment} />
           </div>
           {(data?.comments.length ?? 0) > 0 && (
             <SortSelect
-              currentSortKey={sortKey}
+              currentSortKey={commentsMeta.sort}
               totalSortKeys={sortKeys}
-              onValueChange={handleValueChange}
+              onValueChange={changeSorting}
             />
           )}
           {status === 'pending' && <CommentSkeleton />}
           <div className="flex-1">
             {status === 'success' &&
-              (data.comments.length === 0 ? (
+              (data?.comments.length === 0 ? (
                 <NoComments />
               ) : (
-                data.comments.map((comment) => (
+                data?.comments.map((comment) => (
                   <Comment
+                    key={comment.id}
                     preselectedCommentMetadata={preselectedCommentMetadata}
                     comment={comment}
-                    key={comment.id}
-                    queryKey={queryKey}
                     rootId={rootId}
                     type={type}
+                    deleteComment={deleteComment}
+                    updateComment={updateComment}
                   />
                 ))
               ))}
@@ -188,7 +117,7 @@ export function Comments({ preselectedCommentMetadata, rootId, type, expanded = 
           {(data?.totalPages ?? 0) > 1 && (
             <div className="mt-2 flex justify-center">
               <Pagination
-                currentPage={page}
+                currentPage={commentsMeta.page}
                 onClick={handleChangePage}
                 totalPages={data?.totalPages ?? 0}
               />
