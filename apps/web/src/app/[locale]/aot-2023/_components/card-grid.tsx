4d9dd89986c7f8f2909ce6868d62d5b0de9e87c3
bautistaaa <chrisbautistaaa@gmail.com>
2023-11-21 00:04:24 -0800

    (feat): initial design grid for advent of typescript (#1158)
    * initial
    
    * slowly working
    
    * more
    
    * cleanup
    
    * more
    
    * more
    
    * fix mobile
    
    * more
    
    * initial landing draft
    
    * fix: formatting

--- apps/web/src/app/[locale]/aot-2023/_components/card-grid.tsx
+++ apps/web/src/app/[locale]/aot-2023/_components/card-grid.tsx
@@ -1,30 +1,17 @@
 import { prisma } from '@repo/db';
-import { ChallengeCard } from './challenge-card';
-
-const slugs = [
-  'hello-world',
-  'pick',
-  'awaited',
-  'readonly',
-  'exclude',
-  'parameters',
-  'omit',
-  'pop',
-  'trim-left',
-  'get-return-type',
-  'permutation',
-  'currying-1',
-];
+import { TiltableCard } from './tiltable-card';
+import { auth, type Session } from '@repo/auth/server';
 
 export async function CardGrid() {
-  const challenges = await getChallenges();
+  const session = await auth();
+  const challenges = await getChallenges(session);
   const challengesToReveal = revealItems(challenges);
 
   return (
     <div className="container">
-      <section className="w-[calc(100% + 8rem)] grid grid-cols-1 gap-4 sm:px-8 md:-mx-16 md:grid-cols-2 md:px-0 lg:mx-0 lg:w-full xl:grid-cols-3 2xl:gap-8">
+      <section className="w-[calc(100% + 8rem)] grid grid-cols-[repeat(1,240px)] justify-center gap-4 sm:px-8 md:-mx-16 md:grid-cols-[repeat(3,240px)] md:px-0 lg:mx-0 lg:w-full xl:grid-cols-[repeat(4,240px)] 2xl:gap-8">
         {challengesToReveal?.map((challenge, i) => {
-          return <ChallengeCard key={i} challenge={challenge} />;
+          return <TiltableCard key={challenge.id} index={i} challenge={challenge} />;
         })}
       </section>
     </div>
@@ -32,22 +19,35 @@ export async function CardGrid() {
 }
 
 export type Challenges = NonNullable<Awaited<ReturnType<typeof getChallenges>>>;
-async function getChallenges() {
-  return prisma.challenge.findMany({
-    where: {
-      slug: {
-        in: slugs,
+async function getChallenges(session: Session | null) {
+  const challenges = await prisma.challenge.findMany({
+    take: 25,
+    include: {
+      submission: {
+        where: {
+          userId: session?.user.id ?? '',
+          isSuccessful: true,
+        },
+        select: {
+          isSuccessful: true,
+        },
+        take: 1,
       },
     },
   });
+
+  return challenges.map((challenge) => ({
+    ...challenge,
+    hasSolved: challenge.submission.length > 0,
+  }));
 }
 
 const MS_PER_DAY = 24 * 60 * 60 * 1000;
 function revealItems(items: Challenges) {
-  const startDate: Date = new Date('2023-12-14');
+  const startDate: Date = new Date('2023-11-17');
   const today: Date = new Date();
 
-  const daysPassed = Math.floor((today.getTime() - startDate.getTime()) / MS_PER_DAY) - 1;
+  const daysPassed = Math.floor((today.getTime() - startDate.getTime()) / MS_PER_DAY);
 
   return items.map((item, index) => {
     const isPastOrCurrentDay = index <= daysPassed;
--- apps/web/src/app/[locale]/aot-2023/_components/index.tsx
+++ apps/web/src/app/[locale]/aot-2023/_components/index.tsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
 import { getAllFlags } from '~/utils/feature-flags';
 import { CardGrid } from './card-grid';
+import Image from 'next/image';
 
 export async function AotLandingPage() {
   const featureFlags = await getAllFlags();
@@ -9,16 +10,13 @@ export async function AotLandingPage() {
   return (
     <div className="flex flex-col gap-5 pb-8 md:gap-10 md:py-5">
       <div className="container">
-        <p className="max-w-[69ch] text-lg leading-7 text-neutral-600 dark:text-white/50 sm:px-8 md:px-0">
-          Amidst the{' '}
-          <span className="font-semibold text-red-600 dark:text-red-400">
-            12 Days of TypeScript
-          </span>
-          , my coding workshop turned into a festive frenzy! Bugs were jingled away, interfaces
-          decked with digital tinsel, and puzzling challenges twinkled like digital ornaments! Every
-          win felt like unwrapping a virtual gift, turning those Twelve Days into a tech
-          wonderland—a merry, coding celebration of joy and cheer!
-        </p>
+        <h1 className="mb-10 text-center text-4xl font-bold tracking-tighter text-black dark:text-white sm:text-8xl">
+          <span>Advent</span> of <span className="text-red-600">TypeScript</span>
+        </h1>
+        {/* <div className="flex items-center justify-center gap-10"> */}
+        {/*   <Image src="/aot/deers.png" alt="Deers" height={150} width={130} /> */}
+        {/*   <p className="text-xl font-semibold tracking-tight">asdasdasd</p> */}
+        {/* </div> */}
       </div>
       <CardGrid />
     </div>
