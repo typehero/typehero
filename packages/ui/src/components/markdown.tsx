7b6498305bf268278566c6b81d91fae946dd3317
Heath Henley <heath.j.henley@gmail.com>
2023-11-28 16:08:35 -0500

    Fix ordered list display in markdown (#1175)
    * Copy styles for prose list items to prose-invert
    
    * Switch list subindents
    
    I played with it here: https://remarkjs.github.io/react-markdown/ and
    it didn't seem like react-markdown was parsing the a. sub points
    correctly. Doesn't seem like anything is lost by doing it this way
    instead.
    
    * Fix: add only needed styles in the correct spot
    
    ---------
    
    Co-authored-by: bautistaaa <chrisbautistaaa@gmail.com>

--- packages/ui/src/components/markdown.tsx
+++ packages/ui/src/components/markdown.tsx
@@ -53,6 +53,9 @@ export function Markdown({ children, className }: { children: string; className?
         ul: ({ className, ...props }) => (
           <ul className={clsx(className, 'list-disc ps-10')} {...props} />
         ),
+        ol: ({ className, ...props }) => (
+          <ol className={clsx(className, 'list-decimal ps-10')} {...props} />
+        ),
         h1: ({ className, ...props }) => (
           <h1 className={clsx(className, 'mb-2 pb-2 text-3xl font-bold')} {...props} />
         ),
