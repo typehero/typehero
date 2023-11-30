10ce436202988b8083631af3d3c688676d4fd896
bautistaaa <chrisbautistaaa@gmail.com>
2023-11-29 23:12:40 -0800

    tweaks

--- apps/web/src/app/[locale]/aot-2023/_components/index.tsx
+++ apps/web/src/app/[locale]/aot-2023/_components/index.tsx
@@ -1,13 +1,10 @@
+import { Button } from '@repo/ui/components/button';
+import { Github } from '@repo/ui/icons';
 import { notFound } from 'next/navigation';
 import { getAllFlags } from '~/utils/feature-flags';
+import { CountdownTimer } from '../../_components/countdown-timer';
 import { About } from './about';
 import { CardGrid } from './card-grid';
-import { Github } from '@repo/ui/icons';
-import { Button } from '@repo/ui/components/button';
-import { CountdownTimer } from '../../_components/countdown-timer';
-import { Sled } from './sled';
-import { cn } from '@repo/ui/cn';
-import hohohoStyles from './hohoho.module.css';
 
 export async function AotLandingPage() {
   const featureFlags = await getAllFlags();
@@ -16,15 +13,14 @@ export async function AotLandingPage() {
   return (
     <div className="flex flex-col gap-5 pb-8 md:gap-10 md:py-5">
       <div className="container">
-        <div className="relative mx-auto w-fit">
-          <Sled className={cn('animate-bounce', hohohoStyles.sled)} />
-          <div className="absolute left-0 top-0 -z-10 h-full w-full transform-gpu bg-gradient-to-r from-red-500 via-red-600 to-green-500 opacity-50 blur-3xl" />
+        <div className="relative mx-auto w-fit text-center">
+          <div className="absolute left-0 top-0 -z-10 h-full w-full transform-gpu bg-gradient-to-r from-red-500 via-red-600 to-green-500 opacity-70 blur-3xl sm:opacity-40" />
           <h1 className="mb-10 mt-16 text-4xl font-bold tracking-tighter text-black dark:text-white sm:text-8xl">
             <span>Advent</span> of <span className="text-red-600">TypeScript</span>
           </h1>
         </div>
         <div className="flex flex-col items-center justify-center gap-5">
-          <p className="text-xl font-semibold">
+          <p className="text-center text-xl font-semibold">
             The first type challenge will unlock at{' '}
             <span className="text-primary">midnight(est)</span> on December 1st
           </p>
