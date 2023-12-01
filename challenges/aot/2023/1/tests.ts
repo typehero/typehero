d6591aebf43f4136fc6333932ab217fba68ddd51
bautistaaa <chrisbautistaaa@gmail.com>
2023-11-30 23:13:03 -0800

    fix typo on day 1

--- challenges/aot/2023/1/tests.ts
+++ challenges/aot/2023/1/tests.ts
@@ -3,4 +3,4 @@ import { Expect, Equal } from 'type-testing';
 type test_0_actual = SantasFavoriteCookies;
 //   ^?
 type test_0_expected = 'ginger-bread' | 'chocolate-chip';
-type test_0 = Expect<Equal<test_0_actual, test_0_expected>;
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
