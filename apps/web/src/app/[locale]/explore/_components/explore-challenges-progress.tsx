eaadcee94c51c1fb3930fc6a36d0737205a51771
Raul Rusu <raulrusu46@gmail.com>
2023-12-02 18:25:41 +0200

    feat: show explore challenges completed and total

---
+++ apps/web/src/app/[locale]/explore/_components/explore-challenges-progress.tsx
@@ -0,0 +1,22 @@
+import { CheckCircle } from '@repo/ui/icons';
+
+interface ExploreChallengesProgressionProps {
+  completed: number;
+  total: number;
+}
+
+export async function ExploreChallengesProgression({
+  completed,
+  total,
+}: ExploreChallengesProgressionProps) {
+  return (
+    <>
+      {completed === total && (
+        <CheckCircle className="stroke-green-600 dark:stroke-green-300" size={18} />
+      )}
+      <span className="text-muted-foreground text-sm">
+        {completed}/{total}
+      </span>
+    </>
+  );
+}
--- apps/web/src/app/[locale]/explore/_components/explore.action.ts
+++ apps/web/src/app/[locale]/explore/_components/explore.action.ts
@@ -3,6 +3,7 @@
 import { auth } from '@repo/auth/server';
 import { prisma } from '@repo/db';
 import { Tags, type Difficulty } from '@repo/db/types';
+import { cache } from 'react';
 
 export type ExploreChallengeData = ReturnType<typeof getChallengesByTagOrDifficulty>;
 const allTags: Tags[] = Object.values(Tags);
@@ -53,3 +54,24 @@ export async function getChallengesByTagOrDifficulty(str: string, take?: number)
     }),
   });
 }
+
+/**
+ * Fetches the length of how many challenges are in a
+ * difficutly / tag group
+ * @param str difficutly or tag string
+ */
+export const getExploreChallengesLengthByTagOrDifficulty = cache((str: string) => {
+  const formattedStr = str.trim().toUpperCase();
+
+  return prisma.challenge.count({
+    where: {
+      status: 'ACTIVE',
+      user: {
+        NOT: {
+          status: 'BANNED',
+        },
+      },
+      difficulty: { in: [formattedStr as Difficulty] },
+    },
+  });
+});
