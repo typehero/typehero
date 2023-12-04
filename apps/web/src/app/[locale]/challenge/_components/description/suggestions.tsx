7a3f7a56e1ebea42e7c69cb18792f8e59f8cc99c
Ayush <ayushyadavkr0@tuta.io>
2023-12-05 00:31:57 +0530

    enhancement: added skeleton

--- apps/web/src/app/[locale]/challenge/_components/description/suggestions.tsx
+++ apps/web/src/app/[locale]/challenge/_components/description/suggestions.tsx
@@ -2,16 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { TrackChallenge } from '~/app/[locale]/tracks/_components/track-challenge-card';
 import { getSimilarChallenges } from '~/utils/server/get-similar-challenges';
+import { SuggestionsSkeleton } from './suggestions-skeleton';
 
 export function Suggestions({ challengeId }: { challengeId: number }) {
-  const { data: similarChallenges } = useQuery({
+  const { data: similarChallenges, status } = useQuery({
     queryKey: ['similar-challenges', challengeId],
     queryFn: () => {
       return getSimilarChallenges(challengeId, 3);
     },
   });
 
-  if (!similarChallenges) {
+  if (status === 'error') {
     return null;
   }
 
@@ -20,22 +21,26 @@ export function Suggestions({ challengeId }: { challengeId: number }) {
       <div className="flex items-center justify-between">
         <h3 className="text-foreground text-lg font-semibold md:text-xl">More Challenges</h3>
       </div>
-      <div>
-        {similarChallenges.map((challenge, idx) => {
-          return (
-            <Link href={`/challenge/${challenge.slug}`} key={idx}>
-              <TrackChallenge
-                className="to-neutral-500/10 px-3 py-2 dark:from-neutral-700/70 dark:to-neutral-700/70"
-                challenge={{ ...challenge, submission: [] }}
-                hideSubmissionStatus
-                isCompleted={false}
-                isCompact
-                isInProgress={false}
-              />
-            </Link>
-          );
-        })}
-      </div>
+      {status === 'pending' ? (
+        <SuggestionsSkeleton />
+      ) : (
+        <div>
+          {similarChallenges?.map((challenge, idx) => {
+            return (
+              <Link href={`/challenge/${challenge.slug}`} key={idx}>
+                <TrackChallenge
+                  className="to-neutral-500/10 px-3 py-2 dark:from-neutral-700/70 dark:to-neutral-700/70"
+                  challenge={{ ...challenge, submission: [] }}
+                  hideSubmissionStatus
+                  isCompleted={false}
+                  isCompact
+                  isInProgress={false}
+                />
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
