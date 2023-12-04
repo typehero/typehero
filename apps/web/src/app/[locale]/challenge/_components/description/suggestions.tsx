19946c25f0a329025f58f9b6b9c0b17fae368379
Ayush <ayushyadavkr0@tuta.io>
2023-12-04 15:17:27 +0530

    fix: empty div on no suggestions

--- apps/web/src/app/[locale]/challenge/_components/description/suggestions.tsx
+++ apps/web/src/app/[locale]/challenge/_components/description/suggestions.tsx
@@ -10,36 +10,37 @@ export function Suggestions({ challengeId }: { challengeId: number }) {
       return getSimilarChallenges(challengeId, 3);
     },
   });
+
+  if (!similarChallenges) {
+    return null;
+  }
+
   return (
     <div>
-      {similarChallenges ? (
-        <>
-          <div className="flex items-center justify-between">
-            <h3 className="text-foreground text-lg font-semibold md:text-xl">More Challenges</h3>
-            {/* <Link href="/explore">
+      <div className="flex items-center justify-between">
+        <h3 className="text-foreground text-lg font-semibold md:text-xl">More Challenges</h3>
+        {/* <Link href="/explore">
                 <Button size="sm" className="gap-1 rounded-full" variant="outline">
                   Explore <ChevronRight size={13} />
                 </Button>
               </Link> */}
-          </div>
-          <div>
-            {similarChallenges?.map((challenge, idx) => {
-              return (
-                <Link href={`/challenge/${challenge.slug}`} key={idx}>
-                  <TrackChallenge
-                    className="to-neutral-500/10 px-3 py-2 dark:from-neutral-700/70 dark:to-neutral-700/70"
-                    challenge={{ ...challenge, submission: [] }}
-                    hideSubmissionStatus
-                    isCompleted={false}
-                    isCompact
-                    isInProgress={false}
-                  />
-                </Link>
-              );
-            })}
-          </div>
-        </>
-      ) : null}
+      </div>
+      <div>
+        {similarChallenges?.map((challenge, idx) => {
+          return (
+            <Link href={`/challenge/${challenge.slug}`} key={idx}>
+              <TrackChallenge
+                className="to-neutral-500/10 px-3 py-2 dark:from-neutral-700/70 dark:to-neutral-700/70"
+                challenge={{ ...challenge, submission: [] }}
+                hideSubmissionStatus
+                isCompleted={false}
+                isCompact
+                isInProgress={false}
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
