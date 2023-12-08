c12e9d38e25a0e732cc0caf3c5ab0d152a917fca
__aadamw <131662061+aadamw@users.noreply.github.com>
2023-12-08 23:25:43 +0100

    [Feat] Challenge search (#1259)
    * chore: search component
    
    * chore: update local.md and env.example
    
    * chore: adjust height, remove prefetching
    
    * chore: horizontal styles
    
    * chore: fix eslint
    
    * chore: remove import
    
    * chore: allow closing with cmdk
    
    * chore: wip
    
    * chore: remove grouping by difficulty, add title highlighting
    
    * chore: update design
    
    * chore: add recent searches
    
    * chore: add recent label to recent searches section
    
    * chore: powered by footer
    
    * chore: introduce storage limit
    
    * chore: update algolia colors to match guidelines
    
    ---------
    
    Co-authored-by: bautistaaa <chrisbautistaaa@gmail.com>

--- apps/web/src/components/Navigation/index.tsx
+++ apps/web/src/components/Navigation/index.tsx
@@ -19,6 +19,8 @@ import { getScopedI18n } from '~/locales/server';
 import { MobileNav } from './mobile-nav';
 import { NavLink } from './nav-link';
 import { Badge } from '@repo/ui/components/badge';
+import { Search } from '../search/search';
+import { Suspense } from 'react';
 
 export function getAdminUrl() {
   // reference for vercel.com
@@ -88,6 +90,9 @@ export async function Navigation() {
 
           <div className="flex">
             <div className="flex items-center justify-end gap-2">
+              <Suspense>
+                <Search />
+              </Suspense>
               <ThemeButton />
               {featureFlags?.enableLogin ? <LoginButton /> : null}
               <MobileNav>
