59dae9a20bb18e57c885e54bbe500dd795050f14
Michael Gee <mkgee@g.ucla.edu>
2023-12-10 17:53:22 -0500

    fix(data-table-pagination): address comment

--- packages/ui/src/components/data-table-pagination.tsx
+++ packages/ui/src/components/data-table-pagination.tsx
@@ -32,8 +32,7 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
           </Select>
         </div>
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-          Page {table.getState().pagination.pageIndex + 1} of{' '}
-          {table.getRowModel().rows?.length ? table.getPageCount() : 1}
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
         </div>
         <div className="flex items-center space-x-2">
           <Button
