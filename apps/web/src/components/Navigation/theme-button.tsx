993c2e477784814a4ca0a5fe254018e9227ff293
Amanuel Odicho <amanuelodicho@gmail.com>
2023-12-09 19:54:48 -0600

    Added nullish coalescing to conditionally render button when useTheme as initialized properly

--- apps/web/src/components/Navigation/theme-button.tsx
+++ apps/web/src/components/Navigation/theme-button.tsx
@@ -7,27 +7,29 @@ export function ThemeButton() {
 
   return (
     <>
-      <button
-        aria-label="theme button"
-        className="group rounded-lg p-2 focus:outline-[#2563EB]"
-        onClick={() => {
-          setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-        }}
-        type="button"
-      >
-        {resolvedTheme === 'dark' && (
-          <Moon
-            aria-hidden="true"
-            className="h-5 w-5 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white"
-          />
-        )}
-        {resolvedTheme === 'light' && (
-          <Sun
-            aria-hidden="true"
-            className="h-5 w-5 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white"
-          />
-        )}
-      </button>
+      {resolvedTheme ?? (
+        <button
+          aria-label="theme button"
+          className="group rounded-lg p-2 focus:outline-[#2563EB]"
+          onClick={() => {
+            setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+          }}
+          type="button"
+        >
+          {resolvedTheme === 'dark' && (
+            <Moon
+              aria-hidden="true"
+              className="h-5 w-5 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white"
+            />
+          )}
+          {resolvedTheme === 'light' && (
+            <Sun
+              aria-hidden="true"
+              className="h-5 w-5 duration-150 group-hover:scale-110 group-hover:fill-black dark:group-hover:fill-white"
+            />
+          )}
+        </button>
+      )}
     </>
   );
 }
