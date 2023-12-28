01a0e0dd82c5da8add5ca41e1bf4dbe5f4231625
bautistaaa <chrisbautistaaa@gmail.com>
2023-12-27 16:58:31 -0800

    chore: tweak privacy policy

--- apps/web/src/app/[locale]/(legal)/privacy/page.tsx
+++ apps/web/src/app/[locale]/(legal)/privacy/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
         <p>
           If you have additional questions or require more information about our Privacy Policy, do
           not hesitate to contact us at{' '}
-          <a href="mailto:typeheroapp@gmail.com">typeheroapp@gmail.com</a>.
+          <a href="mailto:support@typehero.dev">support@typehero.dev</a>.
         </p>
 
         <p>
@@ -50,31 +50,16 @@ export default function Page() {
           personal information solely for the purpose of providing support for the internal
           operations of our Services and our Site.
         </p>
-        <p>
-          If you contact us directly, we may receive additional information about you such as your
-          name, email address, phone number, the contents of the message and/or attachments you may
-          send us, and any other information you may choose to provide.
-        </p>
-        <p>
-          When you register for an Account, we may ask for your contact information, including items
-          such as name, company name, address, email address, and telephone number.
-        </p>
 
         <h2>How we use your information</h2>
 
-        <p>We use the information we collect in various ways, including to:</p>
-
         <ul>
           <li>Provide, operate, and maintain our website</li>
-          <li>Improve, personalize, and expand our website</li>
-          <li>Understand and analyze how you use our website</li>
-          <li>Develop new products, services, features, and functionality</li>
           <li>
             Communicate with you, either directly or through one of our partners, including for
             customer service, to provide you with updates and other information relating to the
-            website, and for marketing and promotional purposes
+            website, and for marketing and promotional purposes(opt in).{' '}
           </li>
-          <li>Send you emails</li>
           <li>Find and prevent fraud</li>
         </ul>
 
