7514db73dba3a1f94eb34f1eb5fd18943fd74cbd
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-12-28 21:26:57 +0100

    feat(i18n): cookie banner

--- apps/web/src/components/cookie-banner.tsx
+++ apps/web/src/components/cookie-banner.tsx
@@ -3,8 +3,10 @@
 import { useEffect, useState } from 'react';
 import { Button } from '@repo/ui/components/button';
 import { useLocalStorage } from '~/utils/useLocalStorage';
+import { useScopedI18n } from '~/locales/client';
 
 export const CookieBanner = () => {
+  const t = useScopedI18n('cookieBanner');
   const [mounted, setMounted] = useState(false);
   const [showCookieBanner, setShowCookieBanner] = useLocalStorage('cookie-banner', 'true');
 
@@ -18,17 +20,18 @@ export const CookieBanner = () => {
 
   return (
     <div className="animate-in fade-in bg-background fixed bottom-0 right-0 z-[999] flex w-full flex-col gap-2 rounded-b-none rounded-t-2xl border p-4 sm:bottom-2 sm:right-2 sm:w-[330px] sm:rounded-2xl">
-      <div className="text-center text-xl font-semibold">TypeHero Uses Cookies 🍪</div>
+      <div className="text-center text-xl font-semibold">
+        {t('title')}
+      </div>
       <div className="text-sm">
-        We use cookies to improve your experience. By using TypeHero, you consent to our use of
-        cookies.
+        {t('message')}
       </div>
       <div className="flex gap-2 self-end">
         <Button variant="outline" size="sm" onClick={() => setShowCookieBanner('false')}>
-          Close
+          {t('close')}
         </Button>
         <Button variant="outline" size="sm" onClick={() => setShowCookieBanner('false')}>
-          Ok
+          {t('ok')}
         </Button>
       </div>
     </div>
