ae499a40ad5fd27426b172dd8fdfd66236d80fc2
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-29 20:22:18 +0530

    trimmed down explore-nav and made clean problem-explorer component

--- apps/web/src/components/Navigation/explore-nav.tsx
+++ apps/web/src/components/Navigation/explore-nav.tsx
@@ -1,9 +1,9 @@
 'use client';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { ExploreDrawer } from '~/app/[locale]/challenge/_components/explore-drawer';
 import type { getChallengesByTagOrDifficultyType } from '~/app/[locale]/explore/_components/explore.action';
 import { usePathname } from 'next/navigation';
+import { ProblemExplorer } from './problem-explorer';
 
 export interface AllChallenges {
   popularChallenges: getChallengesByTagOrDifficultyType;
@@ -19,21 +19,11 @@ interface ExploreNavProps {
   title: string;
   allChallenges: AllChallenges;
 }
+
 export function ExploreNav({ href, title, allChallenges }: ExploreNavProps) {
   const pathname = usePathname();
   if (pathname.startsWith('/challenge')) {
-    return (
-      <ExploreDrawer asChild allChallenges={allChallenges}>
-        <div
-          style={{ cursor: 'pointer' }}
-          className={clsx('hover:text-foreground text-foreground/80 transition-colors', {
-            '!text-foreground': pathname === href,
-          })}
-        >
-          {title}
-        </div>
-      </ExploreDrawer>
-    );
+    return <ProblemExplorer allChallenges={allChallenges} />;
   }
   return (
     <Link href={href}>
