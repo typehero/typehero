fc8708611fc52896143fb1c1b149b166ac75715f
Fahad Ahmad <fahadahmad63862@gmail.com>
2023-12-29 20:13:46 +0530

    two new context for setting track and for setting sortKeys, so we dont lose state and avoid prop drilling and clean code

---
+++ apps/web/src/app/problem-explorer.hooks.tsx
@@ -0,0 +1,82 @@
+'use client';
+import { createContext, useState, useContext, type PropsWithChildren } from 'react';
+import type { getChallengesByTagOrDifficultyType } from '~/app/[locale]/explore/_components/explore.action';
+
+interface TrackContextType {
+  getTrack: getChallengesByTagOrDifficultyType | null;
+  setTrack: React.Dispatch<React.SetStateAction<getChallengesByTagOrDifficultyType | null>>;
+}
+
+export const TrackContext = createContext<TrackContextType>({ getTrack: null, setTrack: () => {} });
+
+export const TrackProvider = ({ children }: PropsWithChildren) => {
+  const [getTrack, setTrack] = useState<getChallengesByTagOrDifficultyType | null>(null);
+
+  return <TrackContext.Provider value={{ getTrack, setTrack }}>{children}</TrackContext.Provider>;
+};
+
+export const useTrackContext = () => {
+  const context = useContext(TrackContext);
+  if (context === undefined) {
+    throw new Error('use useTrackContext hook within TrackContextProvider');
+  }
+  return context;
+};
+
+export interface SortKeyType {
+  label: 'Beginner' | 'Easy' | 'Extreme' | 'Hard' | 'Medium' | 'Popular';
+  value: 'beginner' | 'easy' | 'extreme' | 'hard' | 'medium' | 'popular';
+}
+
+export const SORT_KEYS = [
+  {
+    label: 'Popular',
+    value: 'popular',
+  },
+  {
+    label: 'Beginner',
+    value: 'beginner',
+  },
+  {
+    label: 'Easy',
+    value: 'easy',
+  },
+  {
+    label: 'Medium',
+    value: 'medium',
+  },
+  {
+    label: 'Hard',
+    value: 'hard',
+  },
+  {
+    label: 'Extreme',
+    value: 'extreme',
+  },
+] as const;
+
+interface SortingContextType {
+  sortKey: SortKeyType;
+  setSortKey: React.Dispatch<React.SetStateAction<SortKeyType>>;
+}
+
+export const SortingContext = createContext<SortingContextType>({
+  sortKey: SORT_KEYS[0],
+  setSortKey: () => {},
+});
+
+export const SortingProvider = ({ children }: PropsWithChildren) => {
+  const [sortKey, setSortKey] = useState<SortKeyType>(SORT_KEYS[0]);
+
+  return (
+    <SortingContext.Provider value={{ sortKey, setSortKey }}>{children}</SortingContext.Provider>
+  );
+};
+
+export const useSortingContext = () => {
+  const context = useContext(SortingContext);
+  if (context === undefined) {
+    throw new Error('use useSortingContext hook within SortingContextProvider');
+  }
+  return context;
+};
