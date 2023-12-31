60fc80692ff816cbee793269b16f96e54d64007d
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2023-12-31 13:59:37 -0500

    ignoring linting error

--- apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
+++ apps/web/src/app/[locale]/tracks/_components/track-challenge-card.tsx
@@ -51,6 +51,7 @@ export function TrackChallenge({
       : '';
 
   return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label
       htmlFor={challenge.id.toString()}
       className="group/challenge flex cursor-pointer flex-col items-center pt-2"
@@ -133,6 +134,7 @@ export function TrackChallenge({
 export function MockTrackChallenge({ challenge }: { challenge: Challenge }) {
   const isMobile = useIsMobile();
   return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label
       htmlFor={challenge.id.toString()}
       className="group/challenge flex cursor-pointer flex-col items-center pt-2"
