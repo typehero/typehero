6728529ad2d163f5dfb9fdcb7cd73ae5a29867d6
juliusmarminge <julius0216@outlook.com>
2024-01-01 22:42:56 +0100

    fix

--- apps/auth-proxy/routes/[...auth].ts
+++ apps/auth-proxy/routes/[...auth].ts
@@ -1,5 +1,5 @@
 import { Auth } from '@auth/core';
-import Discord from '@auth/core/providers/discord';
+import Github from '@auth/core/providers/github';
 import { eventHandler, toWebRequest } from 'h3';
 
 export default eventHandler(async (event) =>
@@ -8,7 +8,7 @@ export default eventHandler(async (event) =>
     trustHost: !!process.env.VERCEL,
     redirectProxyUrl: process.env.AUTH_REDIRECT_PROXY_URL,
     providers: [
-      Discord({
+      Github({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET,
       }),
