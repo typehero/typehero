e5ecde453bf36a29fb58c5e65d04d3c2a1134bcb
bautistaaa <chrisbautistaaa@gmail.com>
2024-01-04 08:51:17 -0800

    😲

---
+++ apps/admin/src/app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@repo/auth/server';
+import { RoleTypes } from '@repo/db/types';
+import { Lock } from '@repo/ui/icons';
+import { redirect } from 'next/navigation';
+
+export async function Dashboard() {
+  const session = await auth();
+  const roles = session?.user?.role ?? [];
+
+  const isMod = roles.includes(RoleTypes.MODERATOR);
+  const isAdmin = roles.includes(RoleTypes.ADMIN);
+  const isModOrAdmin = isAdmin || isMod;
+  if (isModOrAdmin) redirect('/dashboard/reports');
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <div className="flex min-h-[calc(100dvh-112px)] w-full flex-col items-center justify-center space-y-2">
+          <Lock className="h-8 w-8" />
+          <span className="max-w-[40ch] text-center text-black/50 dark:text-white/50">
+            You do not have permissions to access the page.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/admin/src/app/dashboard/tracks/[trackId]/_actions/update-track.action.ts
+++ apps/admin/src/app/dashboard/tracks/[trackId]/_actions/update-track.action.ts
@@ -1,9 +1,14 @@
 'use server';
 
+import { auth } from '@repo/auth/server';
 import { prisma } from '@repo/db';
+import { assertAdmin } from '~/utils/auth-guards';
 import type { FormSchema } from '../_components/update-track-form';
 
 export async function updateTrack(data: FormSchema & { trackId: number }) {
+  const session = await auth();
+  assertAdmin(session);
+
   const { trackId, trackChallenges, ...rest } = data;
   await prisma.trackChallenge.deleteMany({
     where: { trackId },
