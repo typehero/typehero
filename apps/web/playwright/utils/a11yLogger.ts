8182531a28939ad31cde6eb009deeb376c3ff18f
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-01-06 21:06:30 -0500

    adding custom a11y logger to home.test.ts

--- apps/web/playwright/utils/a11yLogger.ts
+++ apps/web/playwright/utils/a11yLogger.ts
@@ -1,16 +1,25 @@
 import type axe from '@axe-core/playwright/node_modules/axe-core/axe';
 
-function A11yLogger(violations: typeof axe.AxeResults.violations): void {
-  violations.forEach((violation) => {
-    console.log('--------------------------------------------------');
-    console.log('Description: ', violation.description);
-    console.log('Impact: ', violation.impact);
-    console.log('Help: ', violation.help);
-    console.log('Help URL: ', violation.helpUrl);
-    console.log('Failure Summary: \n', violation.nodes[0]?.failureSummary);
-    console.log('HTML: \n', violation.nodes[0]?.html);
-    console.log('--------------------------------------------------');
-  });
+export default class A11yError extends Error {
+  violations: typeof axe.AxeResults.violations = [];
+  constructor(violations: typeof axe.AxeResults.violations) {
+    super();
+    this.violations = violations;
+    this.message = `
+    ==================================================
+    Axe Violations:
+    ==================================================
+    ${violations.map((violation) => {
+      return `
+      Description: ${violation.description}
+      Impact: ${violation.impact}
+      Help: ${violation.help}
+      Help URL: ${violation.helpUrl}
+      Failure Summary: ${violation.nodes[0]?.failureSummary}
+      HTML: ${violation.nodes[0]?.html}
+      ==================================================
+      `;
+    })}
+    `;
+  }
 }
-
-export default A11yLogger;
