a260a92aba6a66c9af03c80423e0038cfee14604
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-28 09:22:00 -0600

    Remove debug logs

--- apps/web/src/app/api/uploadthing/route.ts
+++ apps/web/src/app/api/uploadthing/route.ts
@@ -4,7 +4,4 @@ import { ourFileRouter } from './core';
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
-  config: {
-    logLevel: 'debug',
-  },
 });
