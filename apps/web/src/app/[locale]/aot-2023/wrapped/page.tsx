afff24f36a3efec5fc3c99992aea75615d496e44
Chris Bautista <chrisbautista@netflix.com>
2024-05-30 13:55:42 -0700

    fix: attempt to make wrapped static

--- apps/web/src/app/[locale]/aot-2023/wrapped/page.tsx
+++ apps/web/src/app/[locale]/aot-2023/wrapped/page.tsx
@@ -11,6 +11,7 @@ import { LeastSolved } from './_components/least-solved';
 import { MostSolved } from './_components/most-solved';
 import { UserSummary } from './_components/user-summary';
 
+export const dynamicParams = false;
 export function generateStaticParams() {
   return getStaticParams();
 }
