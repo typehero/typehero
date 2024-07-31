c7cbe16a3d50e04cbf1f0952bff73f9f764822c0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-31 09:55:27 -0500

    LGTM for tailwind

--- apps/aot/next.config.mjs
+++ apps/aot/next.config.mjs
@@ -1,4 +1,49 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  async headers() {
+    return !isProd
+      ? [
+        {
+          // allow CORS only on dev for admin site to get monaco files
+          source: '/min/vs/(.*)',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET' },
+          ],
+        },
+      ]
+      : [];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  transpilePackages: ['@repo/db', '@repo/ui', '@repo/auth', '@repo/monaco'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+    ],
+  },
+  swcMinify: true,
+};
 
 export default nextConfig;
