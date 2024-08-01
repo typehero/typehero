696d185b76813e0e5e88694174c17b43727a9100
PickleNik <31113245+PickleNik@users.noreply.github.com>
2024-08-01 11:11:26 -0400

    a coming soon page (#1472)

--- apps/aot/src/app/layout.tsx
+++ apps/aot/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black text-white">
       <body className={inter.className}>{children}</body>
     </html>
   );
