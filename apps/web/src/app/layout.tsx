0ae7247ad88acf2a9e01626b786d876f5846921a
bautistaaa <chrisbautistaaa@gmail.com>
2024-08-03 21:14:57 -0700

    chore: add new insights package

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from '@repo/ui/components/toaster';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google';
 import { Navigation } from '~/components/Navigation';
 import '../styles/globals.css';
@@ -63,6 +64,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Toaster />
         </Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
