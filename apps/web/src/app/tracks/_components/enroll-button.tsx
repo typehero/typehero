a11e9e9f4d6a8d61bf4b8311ddc6eae6ac946e98
bautistaaa <chrisbautistaaa@gmail.com>
2024-08-05 22:48:01 -0700

    this is broken af

--- apps/web/src/app/tracks/_components/enroll-button.tsx
+++ apps/web/src/app/tracks/_components/enroll-button.tsx
@@ -4,7 +4,6 @@ import { Button } from '@repo/ui/components/button';
 import { toast } from '@repo/ui/components/use-toast';
 import { useState } from 'react';
 import type { enrollUserInTrack, unenrollUserFromTrack } from './track.action';
-import { useProblemExplorerContext } from '~/app/problem-explorer.hooks';
 
 interface EnrollButtonProps {
   action: typeof enrollUserInTrack | typeof unenrollUserFromTrack;
@@ -15,14 +14,12 @@ interface EnrollButtonProps {
 
 export function ActionButton({ action, text, trackId, slug }: EnrollButtonProps) {
   const [isLoading, setIsLoading] = useState(false);
-  const { setIsExplorerDisabled } = useProblemExplorerContext();
   return (
     <Button
       className="shadcnoverridetransition select-none rounded-3xl bg-black text-white shadow-[0px_16px_6px_-16px_#ff4,4px_2px_4px_-2px_#f4f,-4px_2px_4px_-2px_#4f4] hover:-translate-y-1 hover:bg-neutral-800 hover:shadow-[0px_16px_6px_-12px_#ff4,4px_6px_6px_-2px_#f4f,-4px_6px_6px_-2px_#4f4] active:translate-y-0 active:scale-[0.99] active:shadow-[0px_10px_3px_-16px_#ff4,8px_0px_2px_-2px_#f4f,-8px_0px_2px_-2px_#4f4] dark:bg-white dark:text-black dark:hover:bg-white dark:hover:brightness-125 dark:hover:saturate-150"
       disabled={isLoading}
       onClick={async () => {
         setIsLoading(true);
-        setIsExplorerDisabled(text === 'Enroll');
 
         try {
           await action(trackId, slug);
