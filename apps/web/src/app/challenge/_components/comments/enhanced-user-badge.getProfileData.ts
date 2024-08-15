a8588d96ca20ad06bfe82c1d66cac5b8c1a6db3a
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-08-15 22:42:22 +0530

    feat(profile): Add functionality to display user titles and associated icons in EnhancedUserBadge

--- apps/web/src/app/challenge/_components/comments/enhanced-user-badge.getProfileData.ts
+++ apps/web/src/app/challenge/_components/comments/enhanced-user-badge.getProfileData.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { prisma } from '@repo/db';
+import { RoleTypes, type Role } from '@repo/db/types';
 import { getBadges } from '~/app/(profile)/[username]/_components/dashboard/_actions';
 
 export async function getProfileData(username: string) {
@@ -13,14 +14,36 @@ export async function getProfileData(username: string) {
       bio: true,
       id: true,
       name: true,
+      roles: true,
     },
   });
   if (user === null) {
     throw new Error('User not found');
   }
   const badges = await getBadges(user.id);
+  const titles = getTitles({
+    roles: user.roles,
+    isSupporter: true,
+    isContributor: true,
+  });
   return {
     ...user,
+    titles,
     badges: badges.slice(0, 3),
   };
+}
+
+export type TitleInfo = ReturnType<typeof getTitles>[number];
+function getTitles(user: { roles: Role[]; isContributor: boolean; isSupporter: boolean }) {
+  const flairs = [];
+  if (user.roles.find((r) => r.role === RoleTypes.ADMIN)) {
+    flairs.push({ type: 'admin', label: 'Admin' } as const);
+  }
+  if (user.isContributor) {
+    flairs.push({ type: 'contributor', label: 'Wizard' } as const);
+  }
+  if (user.isSupporter) {
+    flairs.push({ type: 'supporter', label: 'Hero' } as const);
+  }
+  return flairs;
 }
--- apps/web/src/app/challenge/_components/comments/enhanced-user-badge.tsx
+++ apps/web/src/app/challenge/_components/comments/enhanced-user-badge.tsx
@@ -4,15 +4,13 @@ import { UserBadge } from '@repo/ui/components/user-badge';
 import { Avatar, AvatarFallback, AvatarImage } from '@repo/ui/components/avatar';
 import Link from 'next/link';
 import { Badge } from '@repo/ui/components/badge';
-import { Sword } from '@repo/ui/icons';
-import {
-  HolidayBronzeBadge,
-  HolidayGoldBadge,
-  HolidayPlatinumBadge,
-} from '~/app/(profile)/[username]/_components/badges/aot-2023-badge';
+import { Shield, Sword, Wand2, type LucideIcon } from '@repo/ui/icons';
+import { HolidayPlatinumBadge } from '~/app/(profile)/[username]/_components/badges/aot-2023-badge';
 import { useQuery } from '@tanstack/react-query';
-import { getProfileData } from './enhanced-user-badge.getProfileData';
+import { getProfileData, type TitleInfo } from './enhanced-user-badge.getProfileData';
 import { useState } from 'react';
+import { SlugToBadgeIcon } from '~/app/(profile)/[username]/_components/dashboard/badges';
+import { type BadgeInfo } from '~/app/(profile)/[username]/_components/dashboard/_actions';
 
 export function EnhancedUserBadge(props: { username: string }) {
   const [enabled, setEnabled] = useState(false);
@@ -32,15 +30,11 @@ export function EnhancedUserBadge(props: { username: string }) {
             <AvatarImage src={query.data?.image ?? ''} />
             <AvatarFallback>DS</AvatarFallback>
           </Avatar>
-          <Badges />
+          {query.isSuccess ? <Badges data={query.data.badges} /> : null}
         </div>
         <div className="space-y-2">
           <h1 className="text-md font-bold">@{props.username}</h1>
-          <div className="flex flex-row space-x-2">
-            <Flair text="Hero" />
-            <Flair text="Wizard" />
-            <Flair text="Admin" />
-          </div>
+          {query.isSuccess ? <Titles data={query.data.titles} /> : null}
           <p className="text-sm ">{query.data?.bio}</p>
         </div>
       </div>
@@ -48,22 +42,37 @@ export function EnhancedUserBadge(props: { username: string }) {
   );
 }
 
-function Flair(props: { text: string }) {
-  console.log('flairs');
+const SlugToTitleIcon: Record<TitleInfo['type'], LucideIcon> = {
+  admin: Shield,
+  supporter: Sword,
+  contributor: Wand2,
+};
+
+function Titles(props: { data: TitleInfo[] }) {
+  console.log({ props });
   return (
-    <Badge className="rounded-md">
-      <Sword className="h-5 w-5 pr-1" />
-      {props.text}
-    </Badge>
+    <div className="flex flex-row space-x-2">
+      {props.data.map((t) => {
+        const Icon = SlugToTitleIcon[t.type];
+        return (
+          <Badge className="rounded-md" key={t.type}>
+            <Icon className="h-5 w-5 pr-1" />
+            {t.label}
+          </Badge>
+        );
+      })}
+    </div>
   );
 }
 
-function Badges() {
+function Badges(props: { data: BadgeInfo[] }) {
   const className = 'h-10 w-10';
   return (
     <div className="flex flex-row space-x-[-15px]">
-      <HolidayGoldBadge className={className} />
-      <HolidayBronzeBadge className={className} />
+      {props.data.map((b) => {
+        const Icon = SlugToBadgeIcon[b.slug];
+        return <Icon className="h-10 w-10" key={b.slug} />;
+      })}
       <HolidayPlatinumBadge className={className} />
     </div>
   );
