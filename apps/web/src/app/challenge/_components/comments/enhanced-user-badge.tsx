67c24cfeffb1b0e164366248ec9a549ea6221351
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-08-28 11:27:21 +0530

    prettier

--- apps/web/src/app/challenge/_components/comments/enhanced-user-badge.tsx
+++ apps/web/src/app/challenge/_components/comments/enhanced-user-badge.tsx
@@ -93,7 +93,7 @@ export function UserBadge(props: UserBadgeProps) {
                   gradient,
                 )}
               >
-                {/* @Mystic_Wanderer_Of_Shadow_Realm_23 */}@{props.user.name}
+                {/* @Mystic_Wanderer_Of_Shadow_Realm_2345678 */}@{props.user.name}
               </h1>
 
               <Titles data={query.data.titles} />
