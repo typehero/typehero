c84378039f03291fa63fddc623818f26fc8c47b3
Shane <shane@swalker.dev>
2024-08-30 21:23:10 -0500

    deleted eslint/prettier config files

--- apps/admin/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: [require.resolve('@repo/lint/next')],
-  ignorePatterns: ['monaco-editor.d.ts'],
-  parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-  },
-};
--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
-};
-
-module.exports = config;
