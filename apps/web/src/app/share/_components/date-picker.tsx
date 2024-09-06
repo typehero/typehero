dc891652dd550a4e17e5ec6fe63c66209583fdbc
zackozack <id.ayushkryadav@gmail.com>
2024-09-07 01:03:52 +0530

    refactor: improve url shortner form

---
+++ apps/web/src/app/share/_components/date-picker.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import * as React from 'react';
+import { format } from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-react';
+
+import { cn } from '@repo/ui/cn';
+import { Button } from '@repo/ui/components/button';
+import { Calendar } from '@repo/ui/components/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@repo/ui/components/popover';
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          className={cn(
+            'ml-2 w-[280px] justify-start text-left font-normal',
+            !date && 'text-muted-foreground',
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, 'PPP') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+      </PopoverContent>
+    </Popover>
+  );
+}
