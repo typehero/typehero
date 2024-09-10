065d699e7bd80eb05a08ad4a6af07bcee0dcc29d
Austin Wiginton <ahwig93@gmail.com>
2024-09-09 21:23:19 -0500

    Run checks:fix script per PR comment

--- apps/web/src/app/challenge/[slug]/submissions/[[...catchAll]]/_components/overview.tsx
+++ apps/web/src/app/challenge/[slug]/submissions/[[...catchAll]]/_components/overview.tsx
@@ -83,7 +83,7 @@ export function SubmissionOverview({ submissionId }: Props) {
           {!isAotChallenge && (
             <div>
               <Link
-                className="bg-primary flex h-8 items-center gap-1 rounded-lg pl-2 pr-3 py-2 text-sm text-white"
+                className="bg-primary flex h-8 items-center gap-1 rounded-lg py-2 pl-2 pr-3 text-sm text-white"
                 href={`/challenge/${slug}/solutions`}
               >
                 <Plus size={16} /> Share your Solution
