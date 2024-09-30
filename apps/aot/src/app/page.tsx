04c283a6289dd563bf812b72e3551945eccfee02
Nikita Krupin <krupin.nikita0110@gmail.com>
2024-09-29 22:16:43 -0400

    hero title

--- apps/aot/src/app/page.tsx
+++ apps/aot/src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Some landing page</div>;
+  return <div className="grid place-content-center min-h-screen">  
+    <div className="flex flex-col container font-black italic">
+      <div className="text-8xl leading-10 w-[35rem]">Advent</div>
+      <div className="text-6xl mx-auto text-center text-blue-400 leading-10 z-10 w-[35rem]">of</div>
+      <div className="text-7xl ml-auto text-right leading-10 w-[35rem]">TypeScript</div>
+    </div>
+  </div>;
 }
