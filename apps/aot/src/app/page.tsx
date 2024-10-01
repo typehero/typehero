60852838c0b22f3b7c192c3a4ba1686a720664c5
Nikita Krupin <krupin.nikita0110@gmail.com>
2024-09-30 20:55:39 -0400

    landing page colors, mobile

--- apps/aot/src/app/page.tsx
+++ apps/aot/src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-  return <div className="grid place-content-center min-h-screen">  
+  return <div className="grid place-content-center min-h-screen bg-gradient-to-b from-emerald-400/40 to-transparent to-50%">  
     <div className="flex flex-col container font-black italic">
-      <div className="text-8xl leading-10 w-[35rem]">Advent</div>
-      <div className="text-6xl mx-auto text-center text-blue-400 leading-10 z-10 w-[35rem]">of</div>
-      <div className="text-7xl ml-auto text-right leading-10 w-[35rem]">TypeScript</div>
+      <div className="text-4xl sm:text-8xl leading-5 sm:leading-10 w-56 sm:w-[40rem]">Advent</div>
+      <div className="text-2xl sm:text-6xl mx-auto text-center text-rose-400 leading-5 sm:leading-10 z-10 w-56 sm:w-[40rem]">of</div>
+      <div className="text-4xl sm:text-8xl ml-auto text-right leading-5 sm:leading-10 w-56 sm:w-[40rem]">TypeScript</div>
     </div>
   </div>;
 }
