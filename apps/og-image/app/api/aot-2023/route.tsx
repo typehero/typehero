ca90ad29db8586dce7c3e6431c60723046c7bfd5
17AnuragMishra <anuragmishra0521@gmail.com>
2024-10-05 05:39:33 +0530

    Updated commit message without sensitive data

--- apps/og-image/app/api/aot-2023/route.tsx
+++ apps/og-image/app/api/aot-2023/route.tsx
@@ -1,20 +1,22 @@
-import { ImageResponse } from "@vercel/og";
+import { ImageResponse } from '@vercel/og';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export async function GET() {
   return new ImageResponse(
-    <div tw="relative flex flex-col">
-      <img
-        src="https://utfs.io/f/7d4db93c-176d-4f25-b82d-8517f6bd053c-5juvoi.png"
-        tw="w-full"
-        alt="gg"
-      />
-      <div tw="absolute font-semibold flex-grow h-full text-5xl text-red-600 flex w-full justify-end">
-        {/* //Todo: Can change to other status programmatically or hardcoded */}
-        {/* <div tw="absolute bottom-0 p-5 px-10">Coming December 1st</div> */}
+    (
+      <div tw="relative flex flex-col">
+        <img
+          src="https://utfs.io/f/7d4db93c-176d-4f25-b82d-8517f6bd053c-5juvoi.png"
+          tw="w-full"
+          alt="gg"
+        />
+        <div tw="absolute font-semibold flex-grow h-full text-5xl text-red-600 flex w-full justify-end">
+          {/* //Todo: Can change to other status programmatically or hardcoded */}
+          {/* <div tw="absolute bottom-0 p-5 px-10">Coming December 1st</div> */}
+        </div>
       </div>
-    </div>,
+    ),
     {
       width: 1200,
       height: 630,
