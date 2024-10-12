352e1ffcc7be5340194f4642221c729a16de405e
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-10-12 14:17:35 +0530

    initial animation

--- apps/web/src/app/(profile-v2)/[username]/v2/_components/moving-grid.tsx
+++ apps/web/src/app/(profile-v2)/[username]/v2/_components/moving-grid.tsx
@@ -5,6 +5,7 @@ import {
   useMotionValue,
   type MotionStyle,
   type MotionValue,
+  useSpring,
 } from 'framer-motion';
 import styles from './moving-grid.module.css';
 import { cn } from '@repo/ui/cn';
@@ -16,25 +17,35 @@ type WrapperStyle = MotionStyle & {
 };
 
 export function MovingGrid(props: React.PropsWithChildren<{}>) {
-  const mouseX = useMotionValue(0);
-  const mouseY = useMotionValue(0);
+  const transition = { damping: 100, stiffness: 1000, type: 'spring' } as const;
+  const mouseX = useMotionValue(43.5);
+  const x = useSpring(mouseX, transition);
+  const mouseY = useMotionValue(80);
+  const y = useSpring(mouseY, transition);
   const isMobile = useIsMobile();
 
   function handleMouseMove(event: React.MouseEvent) {
     if (isMobile) return;
     const boundingRect = event.currentTarget.getBoundingClientRect();
-    mouseX.set(event.clientX - boundingRect.left);
-    mouseY.set(event.clientY - boundingRect.top);
+    x.set(event.clientX - boundingRect.left);
+    y.set(event.clientY - boundingRect.top);
+  }
+
+  function handleMouseLeave() {
+    if (isMobile) return;
+    x.set(43.5);
+    y.set(80);
   }
 
   return (
     <motion.div
       onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
       className={cn('relative overflow-hidden', styles['radial-bg'])}
       style={
         {
-          '--x': useMotionTemplate`${mouseX}px`,
-          '--y': useMotionTemplate`${mouseY}px`,
+          '--x': useMotionTemplate`${x}px`,
+          '--y': useMotionTemplate`${y}px`,
           '--color-center': '#3aecf8',
           '--color-mid': '#5295dc',
           '--color-fade': 'rgba(255, 255, 255, 0)',
