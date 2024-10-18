699c40804148bcf3c881a489024d1f636408183c
Amanuel Odicho <amanuelodicho@gmail.com>
2024-10-18 00:36:27 -0500

    added 1-25 id's for testing

--- apps/aot/src/app/leaderboard/constants.ts
+++ apps/aot/src/app/leaderboard/constants.ts
@@ -1,5 +1,5 @@
 export const LEADERBOARD_RANKING_LIMIT = 10;
 
 export const ADVENT_CHALLENGE_IDS = [
-  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 43, 55, 57, 59, 62, 89, 90,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
 ] as const;
