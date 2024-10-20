f438348a282fbe5d5fed58965f3044710502c098
Amanuel Odicho <amanuelodicho@gmail.com>
2024-10-20 09:11:32 -0500

    ok prettier, I got you

--- packages/db/seed/leaderboard.ts
+++ packages/db/seed/leaderboard.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../src";
+import { prisma } from '../src';
 
 async function generateMockUsers() {
   const users = [];
@@ -12,33 +12,35 @@ async function generateMockUsers() {
   try {
     await prisma.user.createMany({ data: users });
     console.log('Generated 200 mock users');
-  } catch (e) {console.log("mock users already generated")}
+  } catch (e) {
+    console.log('mock users already generated');
+  }
 }
 
 function getRandomTimeAfterFive(day: number) {
-    const date = new Date(`2024-01-${String(day).padStart(2, '0')}T06:00:00.000Z`);
-    
-    const maxAdditionalTime = 18 * 60 * 60 * 1000 - 1;
-    const randomAdditionalTime = Math.floor(Math.random() * maxAdditionalTime);
-    
-    const randomTime = new Date(date.getTime() + randomAdditionalTime);
-    return randomTime;
+  const date = new Date(`2024-01-${String(day).padStart(2, '0')}T06:00:00.000Z`);
+
+  const maxAdditionalTime = 18 * 60 * 60 * 1000 - 1;
+  const randomAdditionalTime = Math.floor(Math.random() * maxAdditionalTime);
+
+  const randomTime = new Date(date.getTime() + randomAdditionalTime);
+  return randomTime;
 }
 
 async function generateMockSubmissions() {
   const users = await prisma.user.findMany();
-  const challenges = Array.from({length: 25}, (_, i) => i+1);
+  const challenges = Array.from({ length: 25 }, (_, i) => i + 1);
   const submissions = [];
 
   for (const user of users) {
     for (const challengeId of challenges) {
-        submissions.push({
-          code: '',
-          createdAt: getRandomTimeAfterFive(challengeId).toISOString(),
-          isSuccessful: true,
-          userId: user.id,
-          challengeId,
-        });
+      submissions.push({
+        code: '',
+        createdAt: getRandomTimeAfterFive(challengeId).toISOString(),
+        isSuccessful: true,
+        userId: user.id,
+        challengeId,
+      });
     }
   }
 
@@ -46,14 +48,12 @@ async function generateMockSubmissions() {
   console.log('Generated random submissions for each user for each challenge');
 }
 
-
-
 try {
-    await generateMockUsers();
-    await generateMockSubmissions();
+  await generateMockUsers();
+  await generateMockSubmissions();
 } catch (e) {
-    console.error(e);
-    process.exit(1);
+  console.error(e);
+  process.exit(1);
 } finally {
-    await prisma.$disconnect();
+  await prisma.$disconnect();
 }
