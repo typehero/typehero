209cbf9a2717e10cf22d027e78b26e7d0a3d13e5
Amanuel Odicho <amanuelodicho@gmail.com>
2024-10-22 19:38:52 -0500

    fixing type of _1 route

--- apps/aot/src/app/leaderboard/day/_1/layout.tsx
+++ apps/aot/src/app/leaderboard/day/_1/layout.tsx
@@ -1,12 +1,9 @@
-import { getCurrentAdventDay } from '~/utils/time-utils';
 import AdventDaysRow from '../../_components/advent-days-row';
 
 export default function LeaderboardLayout({ children }: { children: React.ReactNode }) {
-  const currentAdventDay = getCurrentAdventDay();
-
   return (
     <>
-      <AdventDaysRow currentAdventDay={currentAdventDay} selectedDay={1} />
+      <AdventDaysRow selectedDay={1} />
       {children}
     </>
   );
