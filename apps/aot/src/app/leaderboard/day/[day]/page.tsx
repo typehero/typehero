cc61935ecc35876453066e301d13a4c8e88db55f
Amanuel Odicho <amanuelodicho@gmail.com>
2024-10-22 21:37:23 -0500

    remove async

--- apps/aot/src/app/leaderboard/day/[day]/page.tsx
+++ apps/aot/src/app/leaderboard/day/[day]/page.tsx
@@ -2,7 +2,7 @@ import DailyLeaderboard from '../../_components/daily-leaderboard';
 
 // export const revalidate = 10; Need next.js v15
 
-export default async function Page({ params: { day } }: { params: { day: string } }) {
+export default function Page({ params: { day } }: { params: { day: string } }) {
   const routeDayNum = Number(day);
 
   return <DailyLeaderboard adventDay={routeDayNum} />;
