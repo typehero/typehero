dd131a86fee3c1626a0e712f47ad3b9eb3244522
Amanuel Odicho <amanuelodicho@gmail.com>
2024-10-22 21:30:13 -0500

    removing unneeded html

--- apps/aot/src/app/leaderboard/layout.tsx
+++ apps/aot/src/app/leaderboard/layout.tsx
@@ -2,23 +2,12 @@ import { getCurrentAdventDay, isValidAdventDay } from '~/utils/time-utils';
 import { notFound } from 'next/navigation';
 import { getAllFlags } from '~/utils/feature-flag';
 import { ComingSoon } from '../coming-soon';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default async function LeaderboardLayout({ children }: { children: React.ReactNode }) {
   const { enableAotPlatform } = await getAllFlags();
 
   if (!enableAotPlatform) {
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <main>
-            <ComingSoon />
-          </main>
-        </body>
-      </html>
-    );
+    return <ComingSoon />;
   }
 
   const currentAdventDay = getCurrentAdventDay();
