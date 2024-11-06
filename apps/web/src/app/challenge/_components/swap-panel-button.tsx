a18191fc5444be1e031f5463793352301863816f
nafisreza <bigganinafis@gmail.com>
2024-11-06 09:35:46 +0600

    fix: prettier

--- apps/web/src/app/challenge/_components/swap-panel-button.tsx
+++ apps/web/src/app/challenge/_components/swap-panel-button.tsx
@@ -1,4 +1,4 @@
-import {ArrowRightLeft} from '@repo/ui/icons';
+import { ArrowRightLeft } from '@repo/ui/icons';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@repo/ui/components/tooltip';
 
 interface props {
