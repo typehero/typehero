72a0113fc12101420e28f2dc18d2c3e7bd67014d
pyr33x <pyrexrj@gmail.com>
2024-11-12 15:13:12 +0330

    feat(aot): add 404 & 500 pages

---
+++ apps/aot/src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Snowfall from '~/components/landing/Snowfall';
+import { useEffect } from 'react';
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div className="-mt-14 flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-transparent to-transparent to-50% dark:from-emerald-400/40">
+      <div className="absolute right-[10%] top-1/2 -z-10 hidden h-48 w-[32rem] -translate-y-1/2 rotate-45 animate-pulse rounded-full bg-red-900 blur-3xl xl:block dark:bg-transparent" />
+      <div className="animate-pulse-alternate absolute bottom-1/2 left-[10%] -z-10 hidden h-48 w-[32rem] translate-y-1/2 rotate-45 rounded-full bg-rose-200 blur-3xl xl:block dark:bg-transparent" />
+      <Snowfall />
+      <h1 className="text-center text-6xl font-black text-white">500</h1>
+      <p className="mt-2 text-center text-xl font-medium text-white">
+        An error occurred, maybe refresh?
+      </p>
+    </div>
+  );
+}
---
+++ apps/aot/src/app/not-found.tsx
@@ -0,0 +1,36 @@
+import { Compass, CornerDownLeft } from 'lucide-react';
+import Snowfall from '../components/landing/Snowfall';
+import { Button } from '@repo/ui/components/button';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Not Found | Advent of TypeScript 2024',
+  description: "Couldn't find the requested resource",
+};
+
+export default function Custom404() {
+  return (
+    <div className="-mt-14 flex min-h-screen w-full flex-col items-center justify-center overflow-hidden bg-gradient-to-b from-transparent to-transparent to-50% dark:from-emerald-400/40">
+      <div className="absolute right-[10%] top-1/2 -z-10 hidden h-48 w-[32rem] -translate-y-1/2 rotate-45 animate-pulse rounded-full bg-red-900 blur-3xl xl:block dark:bg-transparent" />
+      <div className="animate-pulse-alternate absolute bottom-1/2 left-[10%] -z-10 hidden h-48 w-[32rem] translate-y-1/2 rotate-45 rounded-full bg-rose-200 blur-3xl xl:block dark:bg-transparent" />
+      <Snowfall />
+      <h1 className="text-center text-6xl font-black text-white">404</h1>
+      <p className="mt-2 text-center text-xl font-medium text-white">
+        Couldn't find the requested resource
+      </p>
+      <div className="mt-8 flex flex-row gap-x-2">
+        <Button disabled variant="outline">
+          <Compass className="mr-2 size-4" />
+          Explore Challenges
+        </Button>
+        <Link href="/">
+          <Button>
+            Return Home
+            <CornerDownLeft className="ml-2 size-4" />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
