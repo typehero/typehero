d3e5a543f6a862babfb3f28e96d78f94c8a3f2dd
Amanuel Odicho <53154523+odicho@users.noreply.github.com>
2024-11-13 09:01:57 -0600

    Make past leaderboard actually build static files + add logic to not use redis for past overall leaderboard (#1637)
    * initial commit
    
    * refactored out of layout.tsx and in page.tsx for year and layout.tsx for leaderboard
    
    * make sure formatDate uses America/New_York
    
    * make sure to not query banned users in leaderboard
    
    * use ACTIVE instead of BANNED status
    
    * fix overall query
    
    * making sure to query db from past leaderboards for overall
    
    * testing build strategy
    
    * testing more exports everywhere lol
    
    * only in layout.tsx
    
    * remove nav work
    
    * remove overall/daily text
    
    * reverting isValidAdventYear to be in page.tsx
    
    * adding imports

--- apps/aot/src/app/events/2023/leaderboard/[day]/page.tsx
+++ apps/aot/src/app/events/2023/leaderboard/[day]/page.tsx
@@ -1,9 +1,5 @@
 import DailyLeaderboard from '../../../[year]/leaderboard/_components/daily-leaderboard';
-import { YEAR, DAY } from '../../date_constants';
-
-export async function generateStaticParams() {
-  return Array.from({ length: DAY }).map((_, index) => ({ day: (index + 1).toString() }));
-}
+import { YEAR } from '../../date_constants';
 
 export default function Page({ params: { day } }: { params: { day: string } }) {
   return <DailyLeaderboard adventYear={YEAR.toString()} adventDay={day} />;
