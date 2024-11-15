c14547b4ece53f27ffd7259fb5b1d0132fd61a7f
bautistaaa <chrisbautistaaa@gmail.com>
2024-11-14 17:41:03 -0800

    fix: og image not showing on routes

--- apps/aot/src/app/events/page.tsx
+++ apps/aot/src/app/events/page.tsx
@@ -2,6 +2,14 @@ import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { api } from '~/trpc/server';
 import { getAllFlags } from '~/utils/feature-flag';
+import { buildMetaForEventPage } from '~/utils/metadata';
+
+export async function generateMetadata() {
+  return buildMetaForEventPage({
+    title: 'Advent of Typescript',
+    description: 'Advent of Typescript',
+  });
+}
 
 export default async function Page() {
   const { enableAotPlatform } = await getAllFlags();
