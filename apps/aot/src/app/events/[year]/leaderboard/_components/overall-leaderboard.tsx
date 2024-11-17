153f7cb0c1ab474b7a568f2910eac15ac117787d
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2024-11-17 22:11:09 +0800

    Update apps/aot/src/app/events/[year]/leaderboard/_components/overall-leaderboard.tsx
    Co-authored-by: Amanuel Odicho <53154523+odicho@users.noreply.github.com>

--- apps/aot/src/app/events/[year]/leaderboard/_components/overall-leaderboard.tsx
+++ apps/aot/src/app/events/[year]/leaderboard/_components/overall-leaderboard.tsx
@@ -54,6 +54,7 @@ export async function getOverallLeaderboard(year: number, isPast: boolean) {
 
   const rankingPromise = prisma.$queryRaw<OverallLeaderboardEntry[]>`
   SELECT
+    u.id as userId
     u.name,
     u.image,
     CAST(SUM(r.points) AS SIGNED) AS score
