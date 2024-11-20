d79744cd8485b4b289ffde247684208d41660f67
bautistaaa <chrisbautistaaa@gmail.com>
2024-11-20 14:56:02 -0800

    chore: add about page

--- apps/aot/src/components/navigation/nav-link.tsx
+++ apps/aot/src/components/navigation/nav-link.tsx
@@ -12,7 +12,7 @@ export function NavLink({ href, title }: { href: string; title: string }) {
         className={clsx(
           'rounded-full px-4 text-black/50 transition-colors duration-300 hover:text-black dark:text-white/80 dark:hover:text-yellow-400',
           {
-            '!text-white': pathname === href,
+            '!text-black dark:!text-white': pathname === href,
           },
         )}
       >
