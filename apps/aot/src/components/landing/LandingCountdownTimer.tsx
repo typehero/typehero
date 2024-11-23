8a606f8d8d6552e925c8a6f38e56a2a345052cd3
Amanuel Odicho <amanuelodicho@gmail.com>
2024-11-23 02:07:29 -0600

    add comment

--- apps/aot/src/components/landing/LandingCountdownTimer.tsx
+++ apps/aot/src/components/landing/LandingCountdownTimer.tsx
@@ -2,5 +2,6 @@ import { LandingCountdownTimerClientComponent } from '../CountdownTimer';
 
 export const LandingCountdownTimer = () => {
   const aotStarted = Date.now() >= new Date(Date.UTC(2024, 11, 1, 5, 0, 0)).getTime();
+  // div prevents layout shift in [year]/page.tsx
   return aotStarted ? <div /> : <LandingCountdownTimerClientComponent />;
 };
