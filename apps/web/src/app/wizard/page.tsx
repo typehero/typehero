8854bbca4d8d37b1c7f4c481cccbbc3c69dbd067
Josh Goldberg <git@joshuakgoldberg.com>
2024-12-01 15:43:17 -0500

    Fix build and lint complaints

--- apps/web/src/app/wizard/page.tsx
+++ apps/web/src/app/wizard/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { buildMetaForDefault } from '~/app/metadata';
 import { Wizard } from './_components';
 
-export function generateMetadata(): Promise<Metadata> {
+export function generateMetadata(): Metadata {
   return buildMetaForDefault({
     title: 'Create a Challenge | TypeHero',
     description: 'Create a challenge on TypeHero and share it with the world!',
