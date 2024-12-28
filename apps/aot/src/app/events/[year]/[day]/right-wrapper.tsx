47d3eadae56b2a69e4cbc2ee7fd4478015d9cf2e
nickbar01234 <nickbar01234@gmail.com>
2024-12-28 22:33:47 +0700

    Enforce submission is only viewable by author for aot

--- apps/aot/src/app/events/[year]/[day]/right-wrapper.tsx
+++ apps/aot/src/app/events/[year]/[day]/right-wrapper.tsx
@@ -22,7 +22,7 @@ export function RightWrapper({ challenge }: RightWrapperProps) {
   if (!challenge) return null;
 
   if (segments[0] === 'submissions' && typeof segments[1] === 'string') {
-    return <SubmissionOverview submissionId={segments[1]} />;
+    return <SubmissionOverview submissionId={segments[1]} userId={session?.user.id ?? ''} />;
   }
 
   // Redirect to solution on successful submission and show suggestions
