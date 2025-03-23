7f7db7c912beb84aa81e2c0a55fd8b1159dba167
GitHub <noreply@github.com>
2025-03-23 00:33:55 +0000

    chore: update contributors

--- apps/web/public/contributors.ts
+++ apps/web/public/contributors.ts
@@ -21,7 +21,7 @@ export const contributors = [
         "type": "User",
         "user_view_type": "public",
         "site_admin": false,
-        "contributions": 470
+        "contributions": 471
     },
     {
         "login": "Hacksore",
