307ce98838bf1c6cdd5624f56285aebec2598a92
bautistaaa <chrisbautistaaa@gmail.com>
2025-05-17 18:32:20 -0700

    chore: remove aot ending message (#1743)
    * chore: remove aot end message
    
    * thanks coderabbit!

--- apps/aot/src/components/DailyCountdownTimer.tsx
+++ apps/aot/src/components/DailyCountdownTimer.tsx
@@ -1,5 +1,4 @@
 import { DailyCountdownTimerClientComponent } from './CountdownTimer';
-import Image from 'next/image';
 
 export const DailyCountdownTimer = () => {
   const nowDate = Date.now();
@@ -8,14 +7,7 @@ export const DailyCountdownTimer = () => {
 
   // This will render one day after the last challenge is released
   if (isAtLeastOneDayPastAotEndDate) {
-    return (
-      <>
-        <p className="text-center text-xl font-semibold">Thats a wrap! See you next year!</p>
-        <p className="text-center text-xl font-semibold">
-          <Image src="/santa_dead.png" width={200} height={200} alt="" className="" />
-        </p>
-      </>
-    );
+    return null;
   }
 
   return <DailyCountdownTimerClientComponent />;
